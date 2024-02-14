d8d2e592b70127dde48c5421b34770b7b6355372
unknown <oubedaf@iesch.org>
2024-02-14 19:47:32 +0100

    clase

---
+++ Entornos de desarrollo/Junit/Cuenta2/Cuenta2.java
@@ -0,0 +1,52 @@
+package Cuenta2;
+
+public class Cuenta2 {
+        //defino dos variables para alojar el número de cuenta y el saldo
+        private String numero; //número de la cuenta bancaria
+        private float saldo; //saldo de la cuenta bancaria en euros
+
+    public Cuenta2(String numCta, float saldoCta){
+        numero = numCta;
+        saldo = saldoCta;
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+
+    public void setNumero(String numCta){
+        this.numero = numCta;
+    }
+
+    public float getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(float saldoCta){
+        this.saldo = saldoCta;
+    }
+
+    public void ingresarDinero(float importe){
+        saldo = saldo + importe;
+    }
+
+    public void extraerDinero(float importe){
+        saldo = saldo - importe;
+
+    }
+
+    public void extraerDinero2(float importe){
+        if (saldo-importe < 0) {
+            throw new java.lang.ArithmeticException("Saldo Negativo");
+        }else
+            saldo = saldo - importe;
+
+    }
+
+    public void mostrarCuenta(){
+        System.out.println("El número de cuenta es: " + getNumero());
+        System.out.println("El saldo es: " + getSaldo() + "€");
+    }
+    
+    
+}
