74e75e4a789a90f035c1a1775e027770944275f9
unknown <oubedaf@iesch.org>
2024-02-23 20:23:08 +0100

    clase

---
+++ Programacion/Java/Ejercicio_Java_Tratamiento_Ficheros/POO/src/TallerÓscarÚbeda/Motos.java
@@ -0,0 +1,4 @@
+package TallerÓscarÚbeda;
+
+public class Motos {
+}
--- Programacion/Java/Ejercicio_Java_Tratamiento_Ficheros/POO/src/TallerÓscarÚbeda/Taller.java
+++ Programacion/Java/Ejercicio_Java_Tratamiento_Ficheros/POO/src/TallerÓscarÚbeda/Taller.java
@@ -12,19 +12,49 @@ public class Taller {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public String getDireccion() {
         return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 
     public String getPropietario() {
         return propietario;
     }
 
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
     public List<String> getClientes() {
         return clientes;
     }
 
+    public void setClientes(List<String> clientes) {
+        this.clientes = clientes;
+    }
+
     public List<String> getVehiculos() {
         return vehiculos;
+    }
+
+    public void setVehiculos(List<String> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+    @Override
+    public String toString() {
+        return "Taller{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", propietario='" + propietario + '\'' +
+                ", clientes=" + clientes +
+                ", vehiculos=" + vehiculos +
+                '}';
     }
 }
