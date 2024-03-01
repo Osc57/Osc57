b969b1d974721f1e4af937675f7b385dbaff1aa5
unknown <oubedaf@iesch.org>
2024-03-01 20:53:30 +0100

    clase

---
+++ Programacion/Java/POO_Taller/src/Humano/Cliente.java
@@ -0,0 +1,27 @@
+package Humano;
+
+import java.util.ArrayList;
+
+public class Cliente extends Persona{
+    private String telefono;
+    private ArrayList<Vehiculo>vehiculos;
+    public Cliente(String dni, String nombre) throws Exception {
+        super(dni, nombre);
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+}
---
+++ Programacion/Java/POO_Taller/src/Interfaces/Identificacion.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface Identificacion {
+    public String identificate();
+}
