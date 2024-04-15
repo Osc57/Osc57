24d6d77b7d4974972173b7cc0e34661b99e67ddd
Oscar <oubedaf@iesch.org>
2024-04-15 20:34:42 +0200

    clase

---
+++ Programacion/Java/EjerciciosSerealizacion/src/main/java/org/iesch/Ej1/Main.java
@@ -0,0 +1,27 @@
+package org.iesch.Ej1;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("Las increíbles aventuras de Peter","Julian Vargallo",1996);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writeValueAsString(book);
+            System.out.println(json);
+
+            objectMapper.writeValue(Paths.get("Libro.json").toFile(), book);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
+}
