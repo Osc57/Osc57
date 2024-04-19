97a08ab8f94ce5717ee0959b3dbdafdb587c8c89
Oscar <oubedaf@iesch.org>
2024-04-19 18:23:55 +0200

    clase

--- Lenguaje de Marcas/UD 8-Vue 3 js/mi-primer-vue/src/App.vue
+++ Lenguaje de Marcas/UD 8-Vue 3 js/mi-primer-vue/src/App.vue
@@ -125,9 +125,10 @@
 ----------------------------------------------------------------------------------------
 -->
 <script setup>
-import { ref } from 'vue';
-const nombre = "Vue 3"
+import { ref, computed } from 'vue';
 
+const nombre = "Vue 3"
+//Voy a convertir el contador en una variable reactiva
   const contador = ref(0);
 
   const incrementar = () => {
@@ -142,11 +143,16 @@ const nombre = "Vue 3"
     contador.value = 0;
   }
 
+  const claseContador = computed(()=>{
+    if ( contador.value === 0 ) return 'zero';
+    return contador.value > 0 ? 'positive' : 'negative';
+  });
+
 
 </script>
 <template>
   <h1>Hola {{ nombre }}</h1>
-  <h2 :class="contador >= 0 ? 'positive' : 'negative'">{{ contador }}</h2>
+  <h2 :class="claseContador">{{ contador }}</h2>
   <button @click="incrementar">Incrementar</button>
   <button @click="decrementar">Decrementar</button>
   <button @click="resetear">Resetear</button>
@@ -161,7 +167,11 @@ const nombre = "Vue 3"
   .positive{
     color: green;
   }
+  .zero{
+    color: peru;
+  }
 </style>
+----------------------------------------------------------------------------------------
 
 
 
