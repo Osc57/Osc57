72a7f50aeb5745bec0279eb5d354b7e31b77026a
Oscar <oubedaf@iesch.org>
2024-04-19 17:40:20 +0200

    clase

--- Lenguaje de Marcas/UD 8-Vue 3 js/mi-primer-vue/src/App.vue
+++ Lenguaje de Marcas/UD 8-Vue 3 js/mi-primer-vue/src/App.vue
@@ -62,6 +62,7 @@
   }
 </style>
 -->
+<!--
 <script setup>
     const name = "Vue 3";
     const arrayFrutas = [
@@ -93,6 +94,30 @@
         </li>
     </ul>
 </template>
+-->
+<script setup>
+    const nombre = "Vue 3";
+
+    //método hacerClick
+    const hacerClick = ( mensaje ) => {
+      console.log( mensaje );
+    }
+
+</script>
+
+<template>
+  <h1>Hola {{ nombre }}</h1>
+  <button v-on:click.left="hacerClick('Click desde el Botón 1')">Boton 1</button>
+  <button @click.middle="hacerClick('Click desde el botón 2')">Botón 2</button>
+  <button @click.right="hacerClick('Click desde el botón 2')">Botón 3</button>
+
+</template>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
 
 
 
