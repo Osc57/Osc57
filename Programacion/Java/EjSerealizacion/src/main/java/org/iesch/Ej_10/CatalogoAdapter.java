54cc021b954aa5f615220a28224f0a3726ec9bde
Oscar <oubedaf@iesch.org>
2024-04-19 20:33:19 +0200

    clase

--- Programacion/Java/EjSerealizacion/src/main/java/org/iesch/Ej_10/CatalogoAdapter.java
+++ Programacion/Java/EjSerealizacion/src/main/java/org/iesch/Ej_10/CatalogoAdapter.java
@@ -1,15 +1,20 @@
 package org.iesch.Ej_10;
 
-import javax.xml.bind.annotation.adapters.XmlAdapter;
+import org.iesch.Ej_5.Book;
+import org.iesch.Ej_7.Author;
 
-public class CatalogoAdapter extends XmlAdapter {
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.Map;
+
+public class CatalogoAdapter extends XmlAdapter<CatalogoAdapter, AdatedMap, Map<Author, Book>> {
+
     @Override
-    public Object unmarshal(Object o) throws Exception {
+    public AdatedMap unmarshal(CatalogoAdapter catalogoAdapter) throws Exception {
         return null;
     }
 
     @Override
-    public Object marshal(Object o) throws Exception {
+    public CatalogoAdapter marshal(AdatedMap adatedMap) throws Exception {
         return null;
     }
 }
