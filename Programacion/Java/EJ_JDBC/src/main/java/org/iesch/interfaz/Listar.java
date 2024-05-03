f45cb3757293a71863ed9f1c949304bfed09c41c
Oscar <oubedaf@iesch.org>
2024-05-03 19:06:17 +0200

    clase

---
+++ Programacion/Java/EJ_JDBC/src/main/java/org/iesch/interfaz/Listar.java
@@ -0,0 +1,35 @@
+package org.iesch.interfaz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Listar {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/alumnos";
+        String user = "root";
+        String password = "1234";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM alumno");
+
+            System.out.println("ID\tNombre\t\tApellidos\t\t\tDirección");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nombre = resultSet.getString("nombre");
+                String apellidos = resultSet.getString("apellidos");
+                String direccion = resultSet.getString("direccion");
+
+                System.out.println(id + "\t" + nombre + "\t\t" + apellidos + "\t\t" + direccion);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database");
+            e.printStackTrace();
+        }
+    }
+}
