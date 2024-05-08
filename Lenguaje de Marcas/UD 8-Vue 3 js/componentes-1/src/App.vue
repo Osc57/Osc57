5bb064f2cbc0968437126858f6e15f97bef595d5
Oscar <oubedaf@iesch.org>
2024-05-08 21:03:50 +0200

    clase

--- Lenguaje de Marcas/UD 8-Vue 3 js/componentes-1/src/App.vue
+++ Lenguaje de Marcas/UD 8-Vue 3 js/componentes-1/src/App.vue
@@ -8,10 +8,10 @@
   <div class="container">
     <h1>Componentes</h1>
     <ButtonCounter/>
-    <BlogPost titulo="Post 01" id="1" body="Esto es mi descripción 01" colorText="primary"/>
-    <BlogPost titulo="Post 02" id="2" body="Esto es mi descripción 02" colorText="warning"/>
-    <BlogPost titulo="Post 03" id="3" body="Esto es mi descripción 03" colorText="danger"/>
-    <BlogPost titulo="Post 04" id="4" body="Esto es mi descripción 04" colorText="success"/>
+    <BlogPost titulo="Post 01" :id="1" body="Esto es mi descripción 01" colorText="primary"/>
+    <BlogPost titulo="Post 02" :id="2" body="Esto es mi descripción 02" colorText="warning"/>
+    <BlogPost titulo="Post 03" :id="3" body="Esto es mi descripción 03" colorText="danger"/>
+    <BlogPost titulo="Post 04" :id="4"/>
 
   </div>
   
