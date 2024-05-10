944f53412bac301983e2ea2480074e3133864489
Oscar <oubedaf@iesch.org>
2024-05-10 18:41:19 +0200

    clase

--- Lenguaje de Marcas/UD 8-Vue 3 js/componentes-1/src/App.vue
+++ Lenguaje de Marcas/UD 8-Vue 3 js/componentes-1/src/App.vue
@@ -5,12 +5,14 @@
 
   // 1 - Me creo un array de objetos
   const posts = ref([
+    
     { titulo: 'Post 01', id: 1, body:'descripcion 01'},
     { titulo: 'Post 02', id: 2, body:'descripcion 02'},
     { titulo: 'Post 03', id: 3, body:'descripcion 03'},
     { titulo: 'Post 04', id: 4, body:'descripcion 04'},
     { titulo: 'Post 05', id: 5, body:'descripcion 05'},
     { titulo: 'Post 06', id: 6, },
+  
   ]);
 
   const favorito = ref('');
