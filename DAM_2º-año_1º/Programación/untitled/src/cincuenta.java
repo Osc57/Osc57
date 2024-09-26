35ef28f9a07ecc4121b412ad786f5db47faafe2b
unknown <oubedaf@iesch.org>
2024-09-26 19:35:02 +0200

    clase

---
+++ DAM_2º-año_1º/Programación/untitled/src/cincuenta.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class cincuenta {
+    public static void main(String[] args) {
+        int n=0;
+        int suma =0;
+
+        while (n < 50){
+            n = n+1;
+            suma = suma + n;
+        }
+        System.out.println(suma);
+    }
+}
