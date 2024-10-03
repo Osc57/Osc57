54ed8567741332289905848d81766c1dcaa9e464
unknown <oubedaf@iesch.org>
2024-10-03 20:40:54 +0200

    clase

--- DAM_2º-año_1º/Programación/Ej1/src/Ej11.java
+++ DAM_2º-año_1º/Programación/Ej1/src/Ej11.java
@@ -3,6 +3,15 @@
 //numérico.
 public class Ej11 {
     public static void main(String[] args) {
+        //Óscar
+
+        char letra1 = 79;
+        char letra2 = 115;
+        char letra3 = 99;
+        char letra4 = 97;
+        char letra5 = 114;
+
+        System.out.println(""+letra1+letra2+letra3+letra4+letra5);
 
     }
 }
