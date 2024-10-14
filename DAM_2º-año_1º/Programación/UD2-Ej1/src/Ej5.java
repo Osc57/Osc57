df971e0b558fbae795e50c6e48f5f0664ffef312
unknown <oubedaf@iesch.org>
2024-10-14 20:07:01 +0200

    clase

--- DAM_2º-año_1º/Programación/UD2-Ej1/src/Ej5.java
+++ DAM_2º-año_1º/Programación/UD2-Ej1/src/Ej5.java
@@ -1,19 +1,35 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * El método ramdom de la clase Math devuelve números reales aleatorios entre 0 y 1.
- * a. Basándote en este comportamiento crea un programa que devuelva un
- * número entero aleatorio entero entre 0 y 100.
- * b. Modifica el programa para pedir al usuario un número entero positivo y el
- * programa devuelva tres números enteros aleatorios entre 0 y el número
- * elegido por el usuario.
- * c. Modifica el programa para pedir al usuario dos números enteros positivos y el
- * programa devuelva tres números enteros aleatorios entre los dos números
- * elegidos por el usuario.
+ *      a. Basándote en este comportamiento crea un programa que devuelva un
+ *      número entero aleatorio entero entre 0 y 100.
+ *      b. Modifica el programa para pedir al usuario un número entero positivo y el
+ *      programa devuelva tres números enteros aleatorios entre 0 y el número
+ *      elegido por el usuario.
+ *      c. Modifica el programa para pedir al usuario dos números enteros positivos y el
+ *      programa devuelva tres números enteros aleatorios entre los dos números
+ *      elegidos por el usuario.
  * **/
 public class Ej5 {
     public static void main(String[] args) {
-        double aleatorio = (int) (Math.random()*100)+1;
+        Scanner scanner = new Scanner(System.in);
+
+        int aleatorio = (int) (Math.random()*100);
+        System.out.println("Número aleatorio entre 1 y 100: " + aleatorio);
+
+        System.out.print("Dime un número: ");
+        int numero = scanner.nextInt();
+
+        int aleatorio2 = (int)(Math.random()*numero);
+        int aleatorio3 = (int)(Math.random()*numero);
+        int aleatorio4 = (int)(Math.random()*numero);
+        System.out.println("Número aleatorio entre 0 y el número elegido: " + aleatorio2);
+        System.out.println("Número aleatorio entre 0 y el número elegido: " + aleatorio3);
+        System.out.println("Número aleatorio entre 0 y el número elegido: " + aleatorio4);
+
+
 
 
     }
