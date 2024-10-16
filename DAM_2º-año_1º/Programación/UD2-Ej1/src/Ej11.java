d9a34b5c561d77d82f580c68f6f01cc1c876707a
unknown <oubedaf@iesch.org>
2024-10-16 16:43:05 +0200

    clase

--- DAM_2º-año_1º/Programación/UD2-Ej1/src/Ej11.java
+++ DAM_2º-año_1º/Programación/UD2-Ej1/src/Ej11.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Crea un programa para procesar datos de viviendas; define las siguientes variables
  * enteras precio y superficie y la variable booleana tieneGaraje. El programa deberá
@@ -11,6 +13,22 @@
  * **/
 public class Ej11 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        boolean tieneGaraje = false;
+        boolean meInteresa = false;
+
+        System.out.print("Dime el precio de la vivienda: ");
+        int precio = scanner.nextInt();
+
+        System.out.print("Dime la superficie de la vivienda: ");
+        int superficie = scanner.nextInt();
+
+        System.out.print("¿Tiene garaje? true(si)/false(no): ");
+        tieneGaraje = scanner.nextBoolean();
+
+        meInteresa = precio < 150000 && superficie > 80 && tieneGaraje;
+
+        System.out.println("¿Me interesa?: " + meInteresa);
+
     }
 }
