622b20b751d44e362dd24e0ad58e5323b52b5cad
Oscar <oubedaf@iesch.org>
2024-10-24 21:13:55 +0200

    clase

---
+++ DAM_2º-año_1º/Programación/UD3-Ej4/src/Ej28.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ *  Crea un programa que "dibuje" un triángulo decreciente, con la altura que
+ *  indique el usuario. Por ejemplo, si el usuario dice que desea 4 caracteres de
+ *  alto, el triángulo sería así:
+ *  ****
+ *  ***
+ *  **
+ *  *
+ *  
+ * **/
+public class Ej28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca la altura del triángulo: ");
+        int altura = sc.nextInt();
+        
+
+    }
+}
