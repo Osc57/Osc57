989fef7ba3db9fe0779054521f41b2ab3263a22d
Oscar <oubedaf@iesch.org>
2024-10-24 21:04:30 +0200

    clase

---
+++ DAM_2º-año_1º/Programación/UD3-Ej2/src/Ej17.java
@@ -0,0 +1,17 @@
+/**
+ * Facturación.
+ *
+ * a. Realiza un programa que pida introducir la facturación de una
+ * empresa para los 12 meses del año y devuelva por pantalla el total
+ * anual al finalizar.
+ * b. Modifica el programa para que devuelva también la media.
+ * c. Modifica el programa para que devuelva el mes con mayor
+ * facturación y el valor de ésta.
+ * d. Modifica el programa para que devuelva el mes con menor
+ * facturación y el valor de ésta.
+ * **/
+public class Ej17 {
+    public static void main(String[] args) {
+        
+    }
+}
