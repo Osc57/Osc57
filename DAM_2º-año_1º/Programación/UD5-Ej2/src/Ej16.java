140f2ebb81956597486efce61ba6d4bc734c14ae
Oscar <oubedaf@iesch.org>
2024-11-11 16:48:21 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej16.java
@@ -0,0 +1,14 @@
+/**
+ * Codifica una función que dado un número entero devuelva un número entero
+ * “dado la vuelta”, es decir con las cifras en orden inverso, pero sin modificar el
+ * signo:
+ * 125  521, -26  -62.
+ * **/
+public class Ej16 {
+    static int ordenInverso(int n){
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
