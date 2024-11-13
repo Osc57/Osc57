0d9f7bbb7deb15055eb8b9d8490c42f45a9eec8a
Oscar <oubedaf@iesch.org>
2024-11-13 17:23:53 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
@@ -0,0 +1,12 @@
+/**
+ * Palíndromo. Un palíndromo es una palabra o expresión que es igual si se lee
+ * de izquierda a derecha que de derecha a izquierda, por ejemplo, radar. Crea
+ * un programa que pida al usuario introducir una palabra o expresión y valide si
+ * es un palíndromo. La comprobación deberá ser independiente de mayúsculas
+ * o minúsculas y de si las vocales llevan a no tildes.
+ * **/
+public class Ej24 {
+    public static void main(String[] args) {
+        
+    }
+}
