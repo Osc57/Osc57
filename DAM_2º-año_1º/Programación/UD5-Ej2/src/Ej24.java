d694f129625807a40031cf2a7ba476887de8b31d
Oscar <oubedaf@iesch.org>
2024-11-14 21:18:08 +0100

    clase

--- DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
+++ DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
@@ -9,23 +9,32 @@ import java.util.Scanner;
  **/
 public class Ej24 {
     static String cadenaInversa(String cadena) {
-        String inversa = "";
+        String palabraInversa = "";
         for (int i = cadena.length() - 1; i >= 0; i--) {
-            inversa = inversa + cadena.charAt(i);
+            palabraInversa = palabraInversa + cadena.charAt(i);
         }
-        return inversa;
+
+        return palabraInversa;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String tildes = "àáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ";
+        String letras = "aaaaaaaceeeeiiiionoooooouuuuyy";
 
         System.out.print("Dime una palabra para comprobar si es un palindromo: ");
         String palabra = scanner.nextLine().toLowerCase();
 
-        if (palabra.equals(cadenaInversa(palabra))) {
+        String palabraLimpia = palabra;
+        for (int i = 0; i < tildes.length(); i++) {
+            palabraLimpia = palabraLimpia.replace(tildes.charAt(i), letras.charAt(i));
+        }
+
+        if (palabraLimpia.equals(cadenaInversa(palabraLimpia))) {
             System.out.println("La palabra " + palabra + " es un palindromo");
         } else {
             System.out.println("La palabra " + palabra + " no es un palindromo");
         }
+
     }
 }
