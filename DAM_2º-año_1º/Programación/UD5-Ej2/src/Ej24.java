f2ffc48e84c32f515fbf4cfac225d1be28aac8e8
Oscar <oubedaf@iesch.org>
2024-11-14 20:13:07 +0100

    clase

--- DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
+++ DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Palíndromo. Un palíndromo es una palabra o expresión que es igual si se lee
  * de izquierda a derecha que de derecha a izquierda, por ejemplo, radar. Crea
@@ -6,7 +8,23 @@
  * o minúsculas y de si las vocales llevan a no tildes.
  * **/
 public class Ej24 {
+    static String cadenaInversa(String cadena){
+        String inversa = "";
+        for (int i = cadena.length() -1 ; i >= 0; i--) {
+            inversa = inversa + cadena.charAt(i);
+        }
+        return inversa;
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Dime una palabra para comprobar si es un palindromo: ");
+        String palabra = scanner.nextLine().toLowerCase();
+
+        if (palabra.equals(cadenaInversa(palabra))){
+            System.out.println("La palabra " + palabra + " es un palindromo");
+        }else {
+            System.out.println("La palabra " + palabra + " no es un palindromo");
+        }
     }
 }
