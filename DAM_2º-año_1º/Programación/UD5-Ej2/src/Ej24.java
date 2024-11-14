de57b4bc5f7100d2f953838f1d76a1a948ddc6da
Oscar <oubedaf@iesch.org>
2024-11-14 20:14:51 +0100

    clase

--- DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
+++ DAM_2º-año_1º/Programación/UD5-Ej2/src/Ej24.java
@@ -6,24 +6,25 @@ import java.util.Scanner;
  * un programa que pida al usuario introducir una palabra o expresión y valide si
  * es un palíndromo. La comprobación deberá ser independiente de mayúsculas
  * o minúsculas y de si las vocales llevan a no tildes.
- * **/
+ **/
 public class Ej24 {
-    static String cadenaInversa(String cadena){
+    static String cadenaInversa(String cadena) {
         String inversa = "";
-        for (int i = cadena.length() -1 ; i >= 0; i--) {
+        for (int i = cadena.length() - 1; i >= 0; i--) {
             inversa = inversa + cadena.charAt(i);
         }
         return inversa;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Dime una palabra para comprobar si es un palindromo: ");
         String palabra = scanner.nextLine().toLowerCase();
 
-        if (palabra.equals(cadenaInversa(palabra))){
+        if (palabra.equals(cadenaInversa(palabra))) {
             System.out.println("La palabra " + palabra + " es un palindromo");
-        }else {
+        } else {
             System.out.println("La palabra " + palabra + " no es un palindromo");
         }
     }
