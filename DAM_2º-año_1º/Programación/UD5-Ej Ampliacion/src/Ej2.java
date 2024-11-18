8f612f65594320c9d29077a49eaada7264374633
Oscar <oubedaf@iesch.org>
2024-11-18 20:25:48 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD5-Ej Ampliacion/src/Ej2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Desarrolla un programa que pida al usuario introducir una frase de manera
+ * indefinida. Una vez introducida el programa mostrará el número total de
+ * veces que aparece la letra ‘s’ (independientemente de que sean mayúsculas y
+ * minúsculas). El programa debe finalizar cuando el usuario introduzca
+ * únicamente un punto (., ASCII 46).
+ * **/
+public class Ej2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dime una frase indefinida: ");
+        String frase = scanner.nextLine().toLowerCase();
+
+        int contador = 0;
+
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == 's'){
+                contador++;
+            }else {
+                if (frase.charAt(i) == '.'){
+                    System.out.println("Números de 's' antes del punto: " + contador);
+                }
+            }
+        }
+        System.out.println("La letra s aparece " + contador + " vez/ces");
+    }
+}
