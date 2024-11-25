aa7843fa5adc8f74f2e14747bccdacc8a9935265
Oscar <oubedaf@iesch.org>
2024-11-25 18:49:17 +0100

    clase

--- DAM_2º-año_1º/Programación/UD5-Ej Ampliacion/src/Ej8.java
+++ DAM_2º-año_1º/Programación/UD5-Ej Ampliacion/src/Ej8.java
@@ -6,28 +6,34 @@ import java.util.Scanner;
  * debe ignorar el signo).
  **/
 public class Ej8 {
-    static int numeroInverso(int num){
+    static int numeroInverso(int num) {
         String cadena = String.valueOf(num);
         String inversa = "";
-        for (int i = cadena.length() -1 ; i >= 0; i--) {
+        for (int i = cadena.length() - 1; i >= 0; i--) {
             inversa = inversa + cadena.charAt(i);
         }
 
         return Integer.parseInt(inversa);
     }
-    static int convertirPositivo  (int n){
-        if (n < 0){
+
+    static int convertirPositivo(int n) {
+        if (n < 0) {
             n = n * -1;
         }
         return n;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Dime un número: ");
         int numero = scanner.nextInt();
-        
-        System.out.println(convertirPositivo(numero));
+
+        if (convertirPositivo(numero) == numeroInverso(convertirPositivo(numero))) {
+            System.out.println("El número " + numero + " es capicua");
+        } else {
+            System.out.println("El número " + numero + " no es capicua");
+        }
 
 
     }
