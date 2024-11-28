559f69679e90895fdff0712eaf51f1adaf2c6c10
Óscar <oubedaf@iesch.org>
2024-11-28 20:33:02 +0100

    clase

--- DAM_2º-año_1º/Programación/UD6-EjPOO/src/Ej2/Alumno.java
+++ DAM_2º-año_1º/Programación/UD6-EjPOO/src/Ej2/Alumno.java
@@ -13,4 +13,37 @@ public class Alumno extends Persona {
     public Alumno(String nombre, String apellidos, LocalDate fNacimiento, String dni, Character sexo) {
         super(nombre, apellidos, fNacimiento, dni, sexo);
     }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public char getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(char grupo) {
+        this.grupo = grupo;
+    }
+
+    public ArrayList<String> getAsignaturas() {
+        return asignaturas;
+    }
+
+    public void setAsignaturas(ArrayList<String> asignaturas) {
+        this.asignaturas = asignaturas;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "curso='" + curso + '\'' +
+                ", grupo=" + grupo +
+                ", asignaturas=" + asignaturas +
+                '}';
+    }
 }
