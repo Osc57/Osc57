e66d9e700997d88b75d9c9fbb9c73906e7099085
Óscar <oubedaf@iesch.org>
2024-12-02 17:15:21 +0100

    clase

--- DAM_2º-año_1º/Programación/UD6-EjPOO/src/Ej4/DemoTicket.java
+++ DAM_2º-año_1º/Programación/UD6-EjPOO/src/Ej4/DemoTicket.java
@@ -6,10 +6,12 @@ public class DemoTicket {
     public static void main(String[] args) {
         Ticket ticket = new Ticket("Teléfono", 1, 449.99, 1.21);
         Ticket ticket1 = new Ticket("Etiquetas NFC", 150, 0.45, 1.21);
+        Ticket ticket2 = new Ticket("Tarjeta Gráfica", 1, 374.99, 1.21);
 
         ArrayList<Ticket> ticketlista = new ArrayList<>();
         ticketlista.add(ticket);
         ticketlista.add(ticket1);
+        ticketlista.add(ticket2);
 
         CalcularTickets calcularTickets = new CalcularTickets(ticketlista);
         calcularTickets.mostrarTicket();
