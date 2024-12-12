0b568a9712372b57c9d54f4b2599b6b72b9e630b
Óscar <oubedaf@iesch.org>
2024-12-12 20:59:10 +0100

    clase

--- DAM_2º-año_1º/Programación/UD8 - Ej1/src/Ej2.java
+++ DAM_2º-año_1º/Programación/UD8 - Ej1/src/Ej2.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Crea un programa que pida al usuario introducir una ruta del directorio de
  * archivos del ordenador. A continuación, el programa debe mostrar cuál es la
@@ -5,9 +8,16 @@
  * mostrar si la ruta existe realmente en el ordenador y, en caso de existir, debe
  * mostrar si se trata de un fichero o un directorio. En el caso de que se trate de
  * un directorio el programa mostrará también el contenido de dicho directorio.
- * **/
+ **/
 public class Ej2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dime la ruta del directorio de archivos del ordenador: ");
+        String ruta = scanner.nextLine();
+
+        File file = new File(ruta);
+
         
     }
 }
