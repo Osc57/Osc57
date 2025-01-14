bbaddd089edbcfe903b619d0fe32c8a98d7786d1
Óscar <oubedaf@iesch.org>
2025-01-14 19:30:30 +0100

    clase

--- DAM_2º-año_1º/Programación/UD8 - Ej4/src/Ej17.java
+++ DAM_2º-año_1º/Programación/UD8 - Ej4/src/Ej17.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
- *
+ *Extraer números de una cadena. Crea una expresión regular que extraiga todos
+ * los números (con o sin decimales) de una cadena de texto. Los números pueden
+ * contener una coma o punto como separador decimal.
+ * • "La temperatura es 23.5 grados y la humedad es 60%" → encuentra 23.5 y
+ * 60
+ * • "La temperatura es veinte grados" → no encuentra nada
  * **/
 public class Ej17 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Dime una temperatura: ");
+        String temperatura = scanner.nextLine();
+
+        String regexTemp = "^[1,]?.?,";
+
+        Pattern patternTemp = Pattern.compile(regexTemp);
     }
 }
