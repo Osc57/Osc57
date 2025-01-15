2954a2133e44635bbd87e86dca0b8101ea38ae3c
Óscar <oubedaf@iesch.org>
2025-01-15 17:27:47 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD8-Ej5/src/Ej22.java
@@ -0,0 +1,9 @@
+/**
+ * Contar las palabras en un archivo. Desarrolla un programa que lea un archivo
+ * de texto y muestre cuántas palabras contiene en total.
+ * **/
+public class Ej22 {
+    public static void main(String[] args) {
+
+    }
+}
