45b71f3f94a349cf1aa222f7d53efdede4174422
Óscar <oubedaf@iesch.org>
2025-01-15 17:07:37 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD8 - Ej4/src/Ej21.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Reemplazar todas las vocales por un asterisco. Escribe una expresión regular
+ * para reemplazar todas las vocales (mayúsculas y minúsculas) en una cadena de texto
+ * por un asterisco (*).
+ * • "Hola Mundo" → "H*l* M*nd*"
+ * • "La casa es grande" → "L* c*s* *s gr*nd*"
+ * **/
+public class Ej21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dime una frase remplazare las vocales por '*': ");
+        String frase = scanner.nextLine();
+
+        String fraseModificada = frase.replaceAll("[AEIOUaeiou]","*");
+
+        System.out.println(fraseModificada);
+
+    }
+}
