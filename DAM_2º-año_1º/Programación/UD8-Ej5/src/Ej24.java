bd48b3905ac177b769b646df5eb9cf703cd07c8d
Óscar <oubedaf@iesch.org>
2025-01-21 17:39:18 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD8-Ej5/src/Ej24.java
@@ -0,0 +1,10 @@
+/**
+ * Invertir el contenido de cada línea. Desarrolla un programa que lea un archivo
+ * de texto y cree un nuevo archivo donde cada línea del texto original esté
+ * invertida (de derecha a izquierda).
+ * **/
+public class Ej24 {
+    public static void main(String[] args) {
+        
+    }
+}
