5ea5bdf3d0a81c1ffe6c00cd59699bd76d190434
Óscar <oubedaf@iesch.org>
2025-01-22 16:09:56 +0100

    clase

--- DAM_2º-año_1º/Programación/UD8-Ej5/src/Ej24.java
+++ DAM_2º-año_1º/Programación/UD8-Ej5/src/Ej24.java
@@ -9,9 +9,15 @@ public class Ej24 {
     public static void main(String[] args) {
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("archivo.txt"));
-             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("archivoInvertido"))) {
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("archivoInvertido.txt"))) {
 
             String linea;
+
+            while ((linea = bufferedReader.readLine()) != null) {
+               String lineaInvertida = new StringBuilder(linea).reverse().toString();
+               bufferedWriter.write(lineaInvertida);
+               bufferedWriter.newLine();
+            }
 
 
         } catch (FileNotFoundException e) {
