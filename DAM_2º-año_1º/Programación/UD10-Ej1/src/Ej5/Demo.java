203e70e92afec3721adc3694d6ffdce3993c09a1
Óscar <oubedaf@iesch.org>
2025-01-30 20:25:31 +0100

    clase

--- DAM_2º-año_1º/Programación/UD10-Ej1/src/Ej5/Demo.java
+++ DAM_2º-año_1º/Programación/UD10-Ej1/src/Ej5/Demo.java
@@ -1,9 +1,5 @@
 package Ej5;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Demo {
     public static void main(String[] args) {
         Usuario usuario1 = new Usuario("Agustin", "micontraseña123");
