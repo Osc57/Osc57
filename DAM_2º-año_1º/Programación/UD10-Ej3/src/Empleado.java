238b4c76e5d9bffcf593e9c37ce5f16acb2b32bc
Óscar <oubedaf@iesch.org>
2025-02-10 18:45:57 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD10-Ej3/src/Empleado.java
@@ -0,0 +1,12 @@
+public class Empleado {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String direccion;
+    private int anosAntiguedad;
+    private int telefContacto;
+    private double salario;
+
+    
+
+}
