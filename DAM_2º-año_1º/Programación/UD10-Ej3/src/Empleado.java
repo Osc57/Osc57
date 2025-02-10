c406d4a5b13012591dc07089f7f6e93ffd910256
Óscar <oubedaf@iesch.org>
2025-02-10 19:50:49 +0100

    clase

--- DAM_2º-año_1º/Programación/UD10-Ej3/src/Empleado.java
+++ DAM_2º-año_1º/Programación/UD10-Ej3/src/Empleado.java
@@ -6,7 +6,29 @@ public class Empleado {
     private int anosAntiguedad;
     private int telefContacto;
     private double salario;
+    private Empleado supervisor;
 
-    
+    public Empleado(String nombre, String apellidos, String dni, String direccion, int anosAntiguedad, int telefContacto, double salario) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.direccion = direccion;
+        this.anosAntiguedad = anosAntiguedad;
+        this.telefContacto = telefContacto;
+        this.salario = salario;
+        this.supervisor = null;
+    }
+
+    public double incrementarSalario(double s) {
+        return this.salario += this.salario * (s / 100);
+    }
+    public Empleado cambiarSupervisor(Empleado nuevoSupervisor){
+        return this.supervisor = nuevoSupervisor;
+    }
+
+    public String imprimir(){
+        return "Nombre: " + nombre + ", apellidos: " + apellidos + ", dni: " + dni +
+                ", dirección: " + direccion + ", antigüedad: " + anosAntiguedad + ", teléfono: " + telefContacto + ", salario: " + salario;
+    }
 
 }
---
+++ DAM_2º-año_1º/Programación/UD10-Ej3/src/Secretario.java
@@ -0,0 +1,20 @@
+public class Secretario extends Empleado{
+    private boolean despacho;
+    private int numFax;
+    private double incrementa = 0.05;
+
+
+    public Secretario(String nombre, String apellidos, String dni, String direccion, int anosAntiguedad, int telefContacto, double salario, boolean despacho, int numFax, double incrementa) {
+        super(nombre, apellidos, dni, direccion, anosAntiguedad, telefContacto, salario);
+        this.despacho = despacho;
+        this.numFax = numFax;
+        this.incrementa = incrementa;
+    }
+
+    @Override
+    public String imprimir() {
+        return super.imprimir() + ", despacho: " + despacho;
+    }
+
+
+}
