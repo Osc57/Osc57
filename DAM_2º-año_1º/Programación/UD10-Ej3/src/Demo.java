cd923797cfa83b488deecfc5a9508a096a8895bc
Óscar <oubedaf@iesch.org>
2025-02-12 17:03:33 +0100

    clase

--- DAM_2º-año_1º/Programación/UD10-Ej3/src/Demo.java
+++ DAM_2º-año_1º/Programación/UD10-Ej3/src/Demo.java
@@ -1,10 +1,16 @@
 public class Demo {
     public static void main(String[] args) {
         Empleado secretario = new Secretario("Agustin","Perez","11111111R","C/Direccion",977777777,1500, true, 78421888);
+        Empleado vendedor = new Vendedor("Manolo","Lama","22222222R","C/Calle",111111111,1850,"1234GRG","Suzuki","Vitara");
+
 
         System.out.println(secretario.imprimir());
         secretario.incrementarSalario();
         System.out.println(secretario.imprimir());
+
+        System.out.println(vendedor.imprimir());
+        vendedor.incrementarSalario();
+        System.out.println(vendedor.imprimir());
 
     }
 }
--- DAM_2º-año_1º/Programación/UD10-Ej3/src/Vendedor.java
+++ DAM_2º-año_1º/Programación/UD10-Ej3/src/Vendedor.java
@@ -1,11 +1,33 @@
 import java.util.ArrayList;
 
-public class Vendedor{
+public class Vendedor extends Empleado{
     private String matricula;
     private String marca;
     private String modelo;
     private int telefono;
     private String areaVenta;
     private ArrayList<String> listaClientes;
+    private double porcentaje;
+    private double incremento = 10;
 
+    public Vendedor(String nombre, String apellidos, String dni, String direccion, int telefContacto, double salario) {
+        super(nombre, apellidos, dni, direccion, telefContacto, salario);
+    }
+
+    public Vendedor(String nombre, String apellidos, String dni, String direccion, int telefContacto, double salario, String matricula, String marca, String modelo) {
+        super(nombre, apellidos, dni, direccion, telefContacto, salario);
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public double incrementarSalario() {
+        return salario = salario + salario * (incremento/100);
+    }
+
+    @Override
+    public String imprimir() {
+        return super.imprimir() + ", Puesto: vendedor" + ", Coche [Marticula: " + matricula + ", Marca: " + marca + ", Modelo: " + modelo + "]";
+    }
 }
