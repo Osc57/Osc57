e532aadddac1ba0aabfeefc7a0be367bbcece511
Óscar <oubedaf@iesch.org>
2025-02-12 16:44:14 +0100

    clase

--- DAM_2º-año_1º/Programación/UD10-Ej3/src/Vendedor.java
+++ DAM_2º-año_1º/Programación/UD10-Ej3/src/Vendedor.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 
 public class Vendedor{
+    private String matricula;
+    private String marca;
+    private String modelo;
     private int telefono;
     private String areaVenta;
-    
+    private ArrayList<String> listaClientes;
+
 }
