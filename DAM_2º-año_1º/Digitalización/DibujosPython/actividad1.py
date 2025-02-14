240bdfbaa2030d21502dd7cab56c9b6a543b1dd0
Óscar <oubedaf@iesch.org>
2025-02-14 18:06:50 +0100

    clase

---
+++ DAM_2º-año_1º/Digitalización/DibujosPython/actividad1.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import *
+
+speed(0)
+
+color("dark green")
+for i in range(72):
+    for j in range(4):
+        forward(100)
+        right(90)
+    right(5)
+
+
+
+turtle.done()
---
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi5.py
@@ -0,0 +1,8 @@
+import turtle
+from turtle import *
+
+for i in range(3):
+    forward(100)
+    left(120)
+
+turtle.done()
