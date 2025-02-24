a79d27f09fe9b0b864b4e97da75e7326dc2489e4
Óscar <oubedaf@iesch.org>
2025-02-24 19:51:29 +0100

    clase

--- DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej1/Demo.java
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej1/Demo.java
@@ -13,20 +13,20 @@ public class Demo extends Registro{
         String regexFrancia=".*,(FR),.*";
         String regexEmailYahoo = ".*,\\w+@yahoo.(\\w+.|\\w+),.*";
 
-        Pattern patternEspanaPortugal = Pattern.compile(regexFrancia);
+        Pattern patternFrancia = Pattern.compile(regexFrancia);
         Pattern patternEmailYahoo = Pattern.compile(regexEmailYahoo);
 
         try ( BufferedReader bufferedReader = new BufferedReader(new FileReader("DATA.csv"));){
 
             String linea  = null;
             while ((linea = bufferedReader.readLine()) != null){
-                if (patternEspanaPortugal.matcher(linea).matches()){
+                if (patternFrancia.matcher(linea).matches()){
                     registro.anadirFrancia(linea);
                 }
                 if (patternEmailYahoo.matcher(linea).matches()){
                     registro.anadirEmail(linea);
                 }
-                System.out.println(linea);
+
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
