2714b15a780a752b1b676feca431a8ddff77dcc1
Óscar <oubedaf@iesch.org>
2025-02-24 20:25:23 +0100

    clase

--- DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej1/Demo.java
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej1/Demo.java
@@ -12,9 +12,11 @@ public class Demo{
 
         String regexFrancia=".*,(FR),.*";
         String regexEmailYahoo = ".*,\\w+@yahoo.(\\w+.|\\w+),.*";
+        String regexLogin = "201(5-(11|12)|6-\\d{2}|7-01)-\\d{2} \\d{2}:\\d{2}:\\d{2}";
 
         Pattern patternFrancia = Pattern.compile(regexFrancia);
         Pattern patternEmailYahoo = Pattern.compile(regexEmailYahoo);
+        Pattern patternLogin = Pattern.compile(regexLogin);
 
         try ( BufferedReader bufferedReader = new BufferedReader(new FileReader("DATA.csv"));){
 
@@ -25,6 +27,10 @@ public class Demo{
                 }
                 if (patternEmailYahoo.matcher(linea).matches()){
                     registro.anadirEmail(linea);
+                }
+                if (patternLogin.matcher(linea).matches()){
+                    registro.anadirLogins(linea);
+                    System.out.println(linea);
                 }
 
             }
