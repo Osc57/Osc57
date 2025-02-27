37b3e4b379802c215e1b63b3c14253c54dd3b38f
Óscar <oubedaf@iesch.org>
2025-02-27 19:59:03 +0100

    clase

--- DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej2/Ej2.java
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej2/Ej2.java
@@ -3,7 +3,9 @@ package Ej2;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
-
+/**
+ * Crea un programa que elija al azar 3 números distintos entre 0 y 9.
+ * **/
 public class Ej2 {
     public static void main(String[] args) {
         Random random = new Random();
