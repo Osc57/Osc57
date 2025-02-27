cd1b2f793f9b531e4c7bb4a2da2d53cd037e747e
Óscar <oubedaf@iesch.org>
2025-02-27 21:10:34 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej4/Ej4.java
@@ -0,0 +1,40 @@
+package Ej4;
+
+import java.util.ArrayList;
+
+/**
+ * Escribe un programa que permita crear una lista de palabras y que, a continuación,
+ * pida dos palabras y sustituya la primera por la segunda en la lista.
+ * Ejemplo:
+ * Lista inicial: “monitor”, “keyboard”, “trackpad”, “computer”
+ * Sustituir la palabra:
+ * “trackpad”
+ * Por:
+ * “mouse”
+ * Lista final: “monitor”, “keyboard”, “mouse”, “computer”
+ * **/
+public class Ej4 {
+    private static void imprimeLaLista(ArrayList<String> l) {
+        for (String componente : l){
+            System.out.print(componente + ", ");
+        }
+        System.out.println("\n");
+    }
+    public static void main(String[] args) {
+        ArrayList<String> listaComponentes = new ArrayList<>();
+
+        listaComponentes.add("monitor");
+        listaComponentes.add("keyboard");
+        listaComponentes.add("trackpad");
+        listaComponentes.add("computer");
+
+        imprimeLaLista(listaComponentes);
+
+        listaComponentes.remove(2);
+        imprimeLaLista(listaComponentes);
+
+        listaComponentes.add(2, "mouse");
+        imprimeLaLista(listaComponentes);
+
+    }
+}
