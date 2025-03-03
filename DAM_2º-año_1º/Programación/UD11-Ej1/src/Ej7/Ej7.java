9a230577b14eb9b8563c0e24f1fc6fb754842cc9
Óscar <oubedaf@iesch.org>
2025-03-03 19:31:13 +0100

    clase

--- DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej7/Ej7.java
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej7/Ej7.java
@@ -1,7 +1,6 @@
 package Ej7;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Escribe un programa que permita crear dos listas de palabras y que, a
@@ -24,11 +23,25 @@ public class Ej7 {
         List<String> list1 = Arrays.asList("Ann", "Carol", "Mary", "Joe", "John", "Kevin", "Jack", "Jack", "Lucy", "Laura", "George", "Ann", "Johnny", "Charlie", "Hillary");
         List<String> list2 = Arrays.asList("Ann", "Martha", "Noel", "Mark", "Pamela", "Elisabeth", "Laura", "Jane", "Mildred", "Laura", "Rue", "Ann", "Jacob", "Sidney", "Sophia", "Tyler");
 
-        for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i).equals(list2.get(i))) {
-                System.out.print(list1.get(i)+ ", ");
-            }
-        }
-        
+        Set<String> list1NoDuplicad = new HashSet<>(list1);
+        Set<String> list2NoDuplicad = new HashSet<>(list2);
+
+        Set<String> listPalabrAmbasList = new HashSet<>(list1NoDuplicad);
+        listPalabrAmbasList.retainAll(list2NoDuplicad);
+
+        System.out.println("Las palabras que hay en ambas listas: " + listPalabrAmbasList);
+
+        Set<String> palabrasList1 = new HashSet<>(list1NoDuplicad);
+        list2NoDuplicad.forEach(palabrasList1::remove);
+        System.out.println("Las palabras que solo aparecen en la primera lista pero no en la segunda: " + palabrasList1);
+
+        Set<String> palabrasList2 = new HashSet<>(list2NoDuplicad);
+        list1NoDuplicad.forEach(palabrasList1::remove);
+        System.out.println("Las palabras que solo aparecen en la segunda lista pero no en la primera: " + palabrasList2);
+
+        Set<String> palabrasAparecenDosList = new HashSet<>(list1NoDuplicad);
+        palabrasAparecenDosList.addAll(list2NoDuplicad);
+        System.out.println("Las palabras que aparecen en alguna de las dos listas: " + palabrasAparecenDosList);
+
     }
 }
