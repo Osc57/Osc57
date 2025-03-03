dc26c0073262deaa26324f930825805c6b577de6
Óscar <oubedaf@iesch.org>
2025-03-03 19:54:57 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej8/Ej8.java
@@ -0,0 +1,22 @@
+package Ej8;
+
+import java.util.Scanner;
+
+/**
+ * Crea un programa que pida al usuario introducir una frase y muestre las letras
+ * ordenadas en orden alfabético junto con el número de veces que aparece dicha
+ * letra en la frase.
+ **/
+
+public class Ej8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dime una frase: ");
+        String frase = scanner.nextLine().toLowerCase();
+
+        for (int i = 0; i < frase.length(); i++) {
+            
+        }
+    }
+}
