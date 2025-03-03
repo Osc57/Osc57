3d16c2b12d00ecb4f63827d8e9b42b1d62ce6d15
Óscar <oubedaf@iesch.org>
2025-03-03 18:58:01 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD11-Ej1/src/Ej7/Ej7.java
@@ -0,0 +1,30 @@
+package Ej7;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Escribe un programa que permita crear dos listas de palabras y que, a
+ * continuación, escriba las siguientes listas (en las que no debe haber repeticiones):
+ * • Lista de palabras que aparecen en las dos listas.
+ * • Lista de palabras que aparecen en la primera lista, pero no en la segunda.
+ * • Lista de palabras que aparecen en la segunda lista, pero no en la primera.
+ * • Lista de palabras que aparecen en alguna de las dos listas.
+ * Ejemplo de listas:
+ * var list1 = new List<string>() {"Ann", "Carol", "Mary", "Joe",
+ * "John", "Kevin", "Jack", "Jack", "Lucy", "Laura", "George",
+ * "Ann", "Johnny", "Charlie", "Hillary"};
+ * var list2 = new List<string>() { "Ann", "Martha", "Noel",
+ * "Mark", "Pamela", "Elisabeth", "Laura", "Jane", "Mildred",
+ * "Laura", "Rue", "Ann", "Jacob", "Sidney", "Sophia", "Tyler" };
+ **/
+
+public class Ej7 {
+    public static void main(String[] args) {
+        List<String> list1 = Arrays.asList("Ann", "Carol", "Mary", "Joe", "John", "Kevin", "Jack", "Jack", "Lucy", "Laura", "George", "Ann", "Johnny", "Charlie", "Hillary");
+        List<String> list2 = Arrays.asList("Ann", "Martha", "Noel", "Mark", "Pamela", "Elisabeth", "Laura", "Jane", "Mildred", "Laura", "Rue", "Ann", "Jacob", "Sidney", "Sophia", "Tyler");
+
+        
+
+    }
+}
