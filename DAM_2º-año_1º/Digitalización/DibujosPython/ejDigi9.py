1f02f1adf1fb2bf41285409c7a0f5cb0b8d1352c
Óscar <oubedaf@iesch.org>
2025-03-07 18:08:37 +0100

    clase

--- DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi9.py
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi9.py
@@ -2,6 +2,6 @@ from tkinter import *
 
 canva = Canvas(width=640, height=480, bg="white")
 canva.pack(expand=YES, fill=BOTH)
-canva.create_oval(200,200,400,400,outline="black",fill="white",width = 2)
+canva.create_oval(200,200,400,400,outline="black",fill="aquamarine",width = 2)
 mainloop()
 
