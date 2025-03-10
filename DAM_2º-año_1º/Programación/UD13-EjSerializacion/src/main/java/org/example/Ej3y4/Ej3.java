3e93541b62123f4d6f6bd7ea2a78ea91aa2b76e1
Óscar <oubedaf@iesch.org>
2025-03-10 17:07:50 +0100

    clase

--- DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej3y4/Ej3.java
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej3y4/Ej3.java
@@ -1,4 +1,27 @@
 package org.example.Ej3y4;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.Ej1y2.Book;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Ej3 {
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.meterLibro(new Book("Veinte Mil Leguas de Viaje Submarino","Julio Verne",1870));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writeValueAsString(library);
+            System.out.println(json);
+
+            objectMapper.writeValue(Paths.get("Librería.json").toFile(), library);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
