c99c7bc46d18bb6c8cf0355bac636709a4d354f3
Óscar <oubedaf@iesch.org>
2025-03-10 20:17:03 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej5/Book.java
@@ -0,0 +1,51 @@
+package org.example.Ej5;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublished=" + yearPublished +
+                '}';
+    }
+}
--- DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej5/Ej5.java
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej5/Ej5.java
@@ -1,24 +1,21 @@
 package org.example.Ej5;
 
-import org.example.Ej1y2.Book;
 
-import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
 
 public class Ej5 {
     public static void main(String[] args) {
-        Book book = new Book("La Vuelta al Mundo en 80", "Julio Verne", 1873);
+        Book book = new Book("La Vuelta al Mundo en 80 días", "Julio Verne", 1873);
 
         try {
             JAXBContext context = JAXBContext.newInstance(Book.class);
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(book, new File("Libro.xml"));
+            marshaller.marshal(book, System.out);
 
-        } catch (JAXBException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
