1cad2da73090ec91c2cd843df65f6b56a50dafa4
Óscar <oubedaf@iesch.org>
2025-03-12 16:11:27 +0100

    clase

--- DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/Ej10.java
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/Ej10.java
@@ -1,7 +1,39 @@
 package org.example.Ej10;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.Ej5.Book;
+import org.example.Ej7.Author;
+import org.example.Ej9.Publisher;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.nio.file.Paths;
+
 public class Ej10 {
     public static void main(String[] args) {
-        
+        LibraryCatalog libraryCatalog = new LibraryCatalog();
+
+        libraryCatalog.anadirLibro(new Author("Julio Verne"), new Book("Veinte Mil Leguas de Viaje Submarino","Julio Verne",1870));
+        libraryCatalog.anadirLibro(new Author("Miguel de Cervantes"), new Book("Don Quijote de La Mancha", "Miguel de Cervantes", 1605));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String json = objectMapper.writeValueAsString(libraryCatalog);
+            System.out.println(json);
+            objectMapper.writeValue(Paths.get("LibrayCatalog.json").toFile(), libraryCatalog);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(LibraryCatalog.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(libraryCatalog, new File("LibraryCatalog.xml"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/LibraryCatalog.java
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/LibraryCatalog.java
@@ -3,9 +3,11 @@ package org.example.Ej10;
 import org.example.Ej5.Book;
 import org.example.Ej7.Author;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.HashMap;
 import java.util.Map;
 
+@XmlRootElement
 public class LibraryCatalog {
     private Map<Author, Book> bookMap = new HashMap<>();
 
@@ -18,5 +20,9 @@ public class LibraryCatalog {
 
     public void setBookMap(Map<Author, Book> bookMap) {
         this.bookMap = bookMap;
+    }
+
+    public void anadirLibro(Author author, Book book){
+        bookMap.put(author,book);
     }
 }
