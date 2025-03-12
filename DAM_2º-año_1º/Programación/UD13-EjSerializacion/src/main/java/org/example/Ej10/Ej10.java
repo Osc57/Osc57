e47a474051a9489848e9bdfd26a3b4ec80cfab3c
Óscar <oubedaf@iesch.org>
2025-03-12 15:55:14 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/Ej10.java
@@ -0,0 +1,7 @@
+package org.example.Ej10;
+
+public class Ej10 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/LibraryCatalog.java
@@ -0,0 +1,5 @@
+package org.example.Ej10;
+
+public class LibraryCatalog {
+    
+}
