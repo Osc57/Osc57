59f25c158b835a27fee7e9cf04e00637eab4ba5e
Óscar <oubedaf@iesch.org>
2025-03-12 16:02:58 +0100

    clase

--- DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/LibraryCatalog.java
+++ DAM_2º-año_1º/Programación/UD13-EjSerializacion/src/main/java/org/example/Ej10/LibraryCatalog.java
@@ -1,5 +1,22 @@
 package org.example.Ej10;
 
+import org.example.Ej5.Book;
+import org.example.Ej7.Author;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LibraryCatalog {
-    
+    private Map<Author, Book> bookMap = new HashMap<>();
+
+    public LibraryCatalog() {
+    }
+
+    public Map<Author, Book> getBookMap() {
+        return bookMap;
+    }
+
+    public void setBookMap(Map<Author, Book> bookMap) {
+        this.bookMap = bookMap;
+    }
 }
