f4cb7809aa28fee958baec5f87daea25a5fefc10
Óscar <oubedaf@iesch.org>
2025-03-14 18:11:30 +0100

    clase

---
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi11.py
@@ -0,0 +1,10 @@
+import turtle
+from tkinter import *
+
+canvas = Canvas(width=800, height=600, bg="white")
+canvas.pack(expand=YES, fill=BOTH)
+
+points = [150,100,200,120,240,180,210,200,150,150,100,200]
+canvas.create_polygon(points,outline="blue",fill="orange", width=2)
+
+turtle.done()
---
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi12.py
@@ -0,0 +1,11 @@
+import turtle
+from tkinter import *
+
+canvas = Canvas(width=800, height=600, bg="white")
+canvas.pack(expand=YES, fill=BOTH)
+
+points = [0,200,100,0,200,200]
+
+canvas.create_polygon(points,outline="blue",fill="green", width=2)
+
+turtle.done()
