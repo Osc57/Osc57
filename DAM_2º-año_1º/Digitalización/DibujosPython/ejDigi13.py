82ee9ba1d79c7915d1f0cd6501a39d6da34b429c
Óscar <oubedaf@iesch.org>
2025-03-14 18:51:57 +0100

    clase

---
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+canvas = Canvas(width=800, height=600, bg="white")
+canvas.pack(expand=YES, fill=BOTH)
+
+points = [100,250,300,170,500,250]
+
+canvas.create_oval(200, 200, 400, 400, outline="white", fill="red", width=1.5)
+canvas.create_polygon(points,outline="blue",fill="blue", width=2)
+
+
+
+
+mainloop()
