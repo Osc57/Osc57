2596555f492bfba9e35b93a74137af53fb5af220
Óscar <oubedaf@iesch.org>
2025-03-18 19:09:04 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej2/Ej2.java
@@ -0,0 +1,26 @@
+package org.example.Ej2;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Ej2 {
+    private static final String url = "jdbc:mysql://localhost:3306/alumnos";
+    private static final String user = "root";
+    private static final String password = "1234";
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection(url,user,password);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO alumno (nombre,apellido,direccion) VALUES (?,?,?)");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
