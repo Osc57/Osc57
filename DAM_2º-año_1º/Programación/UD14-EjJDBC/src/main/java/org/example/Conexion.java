4a2a0a867679a3afb03d0288a9a757dba0ff708e
Óscar <oubedaf@iesch.org>
2025-03-19 15:55:22 +0100

    clase

---
+++ DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Conexion.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String url = "jdbc:mysql://localhost:3306/alumnos";
+    private static final String user = "root";
+    private static final String password = "1234";
+
+    public static Connection connect() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej2/Ej2.java
+++ DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej2/Ej2.java
@@ -1,20 +1,11 @@
 package org.example.Ej2;
 
 
+import org.example.Conexion;
+
 import java.sql.*;
 
-public class Ej2 {
-    private static final String url = "jdbc:mysql://localhost:3306/alumnos";
-    private static final String user = "root";
-    private static final String password = "1234";
-
-    public static Connection connect() {
-        try {
-            return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+public class Ej2 extends Conexion {
 
     public static void main(String[] args) {
 
