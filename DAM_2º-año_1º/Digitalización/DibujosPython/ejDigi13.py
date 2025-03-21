04a393c8e481274bd5d1e2216606fd62816885ee
Óscar <oubedaf@iesch.org>
2025-03-21 17:22:00 +0100

    clase

--- DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
@@ -6,8 +6,11 @@ canvas.pack(expand=YES, fill=BOTH)
 
 points = [100,250,300,170,500,250]
 
-canvas.create_oval(200, 200, 400, 400, outline="white", fill="red", width=1.5)
-canvas.create_polygon(points,outline="blue",fill="blue", width=2)
+canvas.create_oval(200, 200, 400, 400, outline="black", fill="yellow", width=1.5)
+canvas.create_polygon(points,outline="black",fill="brown", width=2)
+
+canvas.create_line(280,290,230,280, width=3)
+canvas.create_line(320,290,370,280, width=3)
 
 
 mainloop()
