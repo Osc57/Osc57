a8da6def5993a52ad39c936ffb9a06991b19b151
Óscar <oubedaf@iesch.org>
2025-03-24 19:07:23 +0100

    clase

--- DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej11/Ej11.java
+++ DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej11/Ej11.java
@@ -1,11 +1,12 @@
 package org.example.Ej11;
 
 /**
- * Realiza un programa que permita matricular a un alumno en un curso. Como
+ * Realiza un programa que permita matricular a un alumno en un curso. Como es lógico
+ * un alumno no puede matricularse dos veces en el mismo curso.
  **/
 
 public class Ej11 {
     public static void main(String[] args) {
-        
+
     }
 }
