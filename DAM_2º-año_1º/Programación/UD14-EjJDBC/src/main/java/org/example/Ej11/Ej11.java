fc1d82dad7aff9c3713c3d204640ef8058b9df9d
Óscar <oubedaf@iesch.org>
2025-03-24 19:16:36 +0100

    clase

--- DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej11/Ej11.java
+++ DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej11/Ej11.java
@@ -1,5 +1,11 @@
 package org.example.Ej11;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+import static org.example.Conexion.connect;
+
 /**
  * Realiza un programa que permita matricular a un alumno en un curso. Como es lógico
  * un alumno no puede matricularse dos veces en el mismo curso.
@@ -7,6 +13,28 @@ package org.example.Ej11;
 
 public class Ej11 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Vamos a matricular a un alumno en un curso");
+        System.out.print("Digame el id de el alumno: ");
+        int idAlumno = scanner.nextInt();
+
+        System.out.print("Digame el id del curso: ");
+        int idCurso = scanner.nextInt();
+
+
+        try (Connection connection = connect();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO matriculado (id_alumno, id_curso) VALUES (?,?);");) {
+
+            preparedStatement.setInt(1, idAlumno);
+            preparedStatement.setInt(2, idCurso);
+
+            preparedStatement.executeUpdate();
+
+            System.out.println("Curso insertado correctamente");
+
+        } catch (Exception e) {
+            System.out.println("Error al insertar el curso: " + e.getMessage());
+        }
     }
 }
