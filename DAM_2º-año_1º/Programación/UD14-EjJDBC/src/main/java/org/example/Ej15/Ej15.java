0893cf41569a403683982438ca96bcc17e5b5264
Óscar <oubedaf@iesch.org>
2025-03-25 19:16:34 +0100

    clase

--- DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej15/Ej15.java
+++ DAM_2º-año_1º/Programación/UD14-EjJDBC/src/main/java/org/example/Ej15/Ej15.java
@@ -1,4 +1,9 @@
 package org.example.Ej15;
+import static org.example.Conexion.connect;
+/***
+ * Realiza un programa que nos liste los cursos existentes, y nos permita seleccionar
+ * uno, ahora nos mostrará los datos de los alumnos matriculados en dicho curso.
+ */
 
 public class Ej15 {
     public static void main(String[] args) {
