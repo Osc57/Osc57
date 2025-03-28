8fbb3ad9dd50256f97c9e11ccc66d902051c3ff0
Óscar <oubedaf@iesch.org>
2025-03-28 18:18:55 +0100

    clase

--- DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
@@ -6,7 +6,7 @@ canvas.pack(expand=YES, fill=BOTH)
 x=200
 y=200
 
-for i in range(40):
+for i in range(80):
     if i % 2 == 0:
         color = "blue"
     else:
@@ -14,8 +14,8 @@ for i in range(40):
 
     canvas.create_text(x,y,text="hola",fill=color,font=("Arial",52))
 
-    y=y+4
-    x=x+4
+    y=y+2
+    x=x+2
 
 
 mainloop()
