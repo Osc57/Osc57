610def2bcba8f6c7e73b94de913c48a5d13c0c61
Óscar <oubedaf@iesch.org>
2025-03-28 18:20:16 +0100

    clase

--- DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
+++ DAM_2º-año_1º/Digitalización/DibujosPython/ejDigi13.py
@@ -3,19 +3,18 @@ from tkinter import *
 canvas = Canvas(width=800, height=600, bg="white")
 canvas.pack(expand=YES, fill=BOTH)
 
-x=200
-y=200
+x = 200
+y = 200
 
 for i in range(80):
     if i % 2 == 0:
         color = "blue"
     else:
-        color="red"
+        color = "red"
 
-    canvas.create_text(x,y,text="hola",fill=color,font=("Arial",52))
+    canvas.create_text(x, y, text="hola", fill=color, font=("Arial", 52))
 
-    y=y+2
-    x=x+2
-
+    y = y + 2
+    x = x + 2
 
 mainloop()
