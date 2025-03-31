790be4cdca3e422e3df624d47d6aa24cd2598e7a
Óscar <oubedaf@iesch.org>
2025-03-31 19:53:34 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/SerializacionDeserealizacion/Ej3/Configuracion.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/SerializacionDeserealizacion/Ej3/Configuracion.java
@@ -1,4 +1,40 @@
 package org.example.SerializacionDeserealizacion.Ej3;
 
 public class Configuracion {
+    private String tema;
+    private String idioma;
+    private int volumen;
+
+    public Configuracion() {
+    }
+
+    public Configuracion(String tema, String idioma, int volumen) {
+        this.tema = tema;
+        this.idioma = idioma;
+        this.volumen = volumen;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
 }
