fe3f418e193bc372f4175df349b954f146c51343
Óscar <oubedaf@iesch.org>
2025-03-31 18:11:15 +0200

    clase

---
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/SerializacionDeserealizacion/Ej2/Ej2.java
@@ -0,0 +1,13 @@
+package org.example.SerializacionDeserealizacion.Ej2;
+
+/**
+ *  Define una clase Producto con atributos id, nombre y precio.
+ *  Crea una lista de objetos Producto y conviértela a JSON y XML.
+ *  Guarda los archivos y recupéralos deserializando su contenido.
+ **/
+
+public class Ej2 {
+    public static void main(String[] args) {
+
+    }
+}
