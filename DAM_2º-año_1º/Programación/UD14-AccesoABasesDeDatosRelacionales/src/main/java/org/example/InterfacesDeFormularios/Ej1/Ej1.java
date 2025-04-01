aa885aeaec8adbcf65aba7a02e46a35414879222
Óscar <oubedaf@iesch.org>
2025-04-01 18:59:29 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej1/Ej1.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej1/Ej1.java
@@ -6,6 +6,7 @@ public class Ej1 {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Hola mundo");
         jFrame.setSize(400,200);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         jFrame.add(new JLabel("Hola mundo"));
 
--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej2/Ej2.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej2/Ej2.java
@@ -1,7 +1,15 @@
 package org.example.InterfacesDeFormularios.Ej2;
 
+import javax.swing.*;
+
 public class Ej2 {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Hola mundo");
+        jFrame.setSize(400,800);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        
+
+        jFrame.setVisible(true);
     }
 }
