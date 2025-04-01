8d57cc7a93262dca4fd502c3d37775982a16e924
Óscar <oubedaf@iesch.org>
2025-04-01 18:57:12 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej1/Ej1.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej1/Ej1.java
@@ -1,7 +1,14 @@
 package org.example.InterfacesDeFormularios.Ej1;
 
+import javax.swing.*;
+
 public class Ej1 {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Hola mundo");
+        jFrame.setSize(400,200);
 
+        jFrame.add(new JLabel("Hola mundo"));
+
+        jFrame.setVisible(true);
     }
 }
