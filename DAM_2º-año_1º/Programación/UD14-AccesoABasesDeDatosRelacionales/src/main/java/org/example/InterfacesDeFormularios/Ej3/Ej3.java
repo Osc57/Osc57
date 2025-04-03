83558c4e0d2f543f1e4e4fd73bcebc8a4de35a36
Óscar <oubedaf@iesch.org>
2025-04-03 20:39:47 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej3/Ej3.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej3/Ej3.java
@@ -1,7 +1,34 @@
 package org.example.InterfacesDeFormularios.Ej3;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Ej3 {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Hola mundo");
+        jFrame.setLayout(new GridLayout(2,1));
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JPanel jPanel1 = new JPanel();
+        JLabel user = new JLabel("Usuario: ");
+        JTextField usuario = new JTextField(20);
+        jPanel1.add(user);
+        jPanel1.add(usuario);
+
+        JButton jButton = new JButton("Botón");
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(jFrame,"Hola " + usuario.getText());
+            }
+        });
+
+
+        jFrame.add(jPanel1);
+        jFrame.add(jButton);
+        jFrame.setVisible(true);
     }
 }
