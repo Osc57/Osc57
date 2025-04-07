b52f620139fa1ee89833a995658e07341a32bf05
Óscar <oubedaf@iesch.org>
2025-04-07 19:23:39 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej5/Ej5.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej5/Ej5.java
@@ -1,7 +1,20 @@
 package org.example.InterfacesDeFormularios.Ej5;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
 public class Ej5 {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Hola mundo");
+        jFrame.setLayout(new GridLayout(3,1));
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        DefaultTableModel modelo = new DefaultTableModel();
+        JTable jt = new JTable(modelo);
+
+        jFrame.add(jt);
+        jFrame.setVisible(true);
     }
 }
