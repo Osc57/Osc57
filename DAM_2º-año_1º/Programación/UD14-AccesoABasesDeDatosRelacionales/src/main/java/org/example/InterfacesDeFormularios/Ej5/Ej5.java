c687c3cfe28f43a849bbbff77b1a1c5c1b9ecf3e
Óscar <oubedaf@iesch.org>
2025-04-07 19:41:29 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej5/Ej5.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej5/Ej5.java
@@ -3,18 +3,56 @@ package org.example.InterfacesDeFormularios.Ej5;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.example.InterfacesDeFormularios.BBDD.connect;
 
 public class Ej5 {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Hola mundo");
-        jFrame.setLayout(new GridLayout(3,1));
+        jFrame.setLayout(new GridLayout(2, 1));
         jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         DefaultTableModel modelo = new DefaultTableModel();
+
+        modelo.addColumn("Id");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Email");
+
         JTable jt = new JTable(modelo);
+        JScrollPane scrollPane = new JScrollPane(jt);
+
+        JButton jButton = new JButton("Actualizar");
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try (Connection connection = connect();
+                     PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM usuario;")) {
+
+                    ResultSet resultSet = preparedStatement.executeQuery();
+
+                    while (resultSet.next()){
+                        modelo.addRow(new Object[]{
+                                resultSet.getInt("id"),
+                                resultSet.getString("nombre"),
+                                resultSet.getString("email")
+                        });
+                    }
+
+                } catch (
+                        Exception exception) {
+                    JOptionPane.showMessageDialog(jFrame, "Error al cargar datos " + exception.getMessage());
+                }
+            }
+        });
 
         jFrame.add(jt);
+        jFrame.add(jButton);
         jFrame.setVisible(true);
     }
 }
