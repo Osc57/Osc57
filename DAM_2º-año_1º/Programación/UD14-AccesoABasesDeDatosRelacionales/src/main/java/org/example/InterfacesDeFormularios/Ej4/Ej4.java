8ba8dfc1729047283e0b4a51ecbf8f85c06835cc
Óscar <oubedaf@iesch.org>
2025-04-07 18:20:13 +0200

    clase

--- DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej4/Ej4.java
+++ DAM_2º-año_1º/Programación/UD14-AccesoABasesDeDatosRelacionales/src/main/java/org/example/InterfacesDeFormularios/Ej4/Ej4.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import static org.example.InterfacesDeFormularios.BBDD.connect;
 
 public class Ej4 {
     public static void main(String[] args) {
@@ -28,11 +32,25 @@ public class Ej4 {
         jButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try (Connection connection = connect();
+                     PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO usuario (nombre,email) VALUES (?,?);")) {
 
+                    preparedStatement.setString(1, usuario.getText() );
+                    preparedStatement.setString(2, contrasena.getText());
+
+                    if (!usuario.getText().isEmpty() || !contrasena.getText().isEmpty()){
+                        preparedStatement.executeUpdate();
+                        JOptionPane.showMessageDialog(jFrame,"Usuario insertado correctamente");
+                    }
+
+                } catch (Exception exception) {
+                    JOptionPane.showMessageDialog(jFrame,"Error al insertar el usuario " + exception.getMessage());
+                }
             }
         });
 
         jFrame.add(jPanel1);
+        jFrame.add(jPanel2);
         jFrame.add(jButton);
         jFrame.setVisible(true);
     }
