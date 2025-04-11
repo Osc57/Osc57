971a5ef20f6d9409d778ff4173cedbd7e6327069
Óscar <oubedaf@iesch.org>
2025-04-11 18:18:18 +0200

    clase

--- DAM_2º-año_1º/Programación/Proyecto/src/main/java/org/example/Interfaces/InterfazLogin1.java
+++ DAM_2º-año_1º/Programación/Proyecto/src/main/java/org/example/Interfaces/InterfazLogin1.java
@@ -1,55 +1,61 @@
 package org.example.Interfaces;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class InterfazLogin1 extends JFrame {
     private String usuario = "Admin";
     private String password = "1234";
     private Font colortextoButton = new Font("Arial", Font.PLAIN, 18);
+    private JPanel jPanel;
+    private JTextField jTextField;
+    private JButton jButton;
+    private JPasswordField jPasswordField;
 
     public InterfazLogin1() {
         this.setLayout(new GridLayout(4, 2));
         this.setTitle("Login");
-        this.setSize(500, 500);
+        this.setSize(500, 450);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel = new JPanel();
+        jPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(jPanel);
+        jPanel.setLayout(null);
+
         JLabel textoLabel = new JLabel("LOGIN ");
         textoLabel.setFont(new Font("Arial", Font.PLAIN, 60));
-        this.add(textoLabel);
+        jPanel.add(textoLabel);
 
         //--------------------------------------------------------------------------------------------------------------
-
-        JPanel panelUsuario = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
         JLabel user = new JLabel("USUARIO:");
-        panelUsuario.add(user, BorderLayout.WEST);
+        jPanel.add(user, BorderLayout.WEST);
         user.setFont(new Font("Arial", Font.PLAIN, 25));
         user.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 67));
 
-        JTextField usuarioTexto = new JTextField();
-        usuarioTexto.setBounds(481, 170, 281, 68);
-        panelUsuario.add(usuarioTexto);
-        usuarioTexto.setColumns(15);
+        jTextField = new JTextField();
+        jTextField.setBounds(481, 170, 281, 68);
+        jPanel.add(jTextField);
+        jTextField.setColumns(15);
 
         //--------------------------------------------------------------------------------------------------------------
 
-        JPanel panelPass = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
         JLabel passw = new JLabel("CONTRASEÑA:");
-        panelPass.add(passw, BorderLayout.WEST);
+        jPanel.add(passw);
         passw.setFont(new Font("Arial", Font.PLAIN, 25));
         passw.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 67));
 
 
-        JPasswordField passwordField = new JPasswordField(15);
-        panelPass.add(passwordField, BorderLayout.CENTER);
+        jPasswordField = new JPasswordField();
+        jPanel.add(jPasswordField, BorderLayout.CENTER);
 
         //--------------------------------------------------------------------------------------------------------------
+        JButton botonIniciar = new JButton("Acceder");
 
-        this.add(panelUsuario);
-        this.add(panelPass);
+
+        this.add(jPanel);
 
     }
 
