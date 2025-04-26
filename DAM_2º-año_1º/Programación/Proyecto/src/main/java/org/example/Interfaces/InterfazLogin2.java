1266da82201a2ec98ce9235764d14d8ec8de7704
Oscar Ubeda <oubedaf@iesch.org>
2025-04-26 23:20:20 +0200

    clase

--- DAM_2º-año_1º/Programación/Proyecto/src/main/java/org/example/Interfaces/InterfazLogin2.java
+++ DAM_2º-año_1º/Programación/Proyecto/src/main/java/org/example/Interfaces/InterfazLogin2.java
@@ -21,7 +21,7 @@ public class InterfazLogin2 extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout(0, 10));
-        
+
         JPanel panelArriba = new JPanel();
         panelArriba.setBorder(BorderFactory.createEmptyBorder(15, 0, 15, 0));
         JLabel jLabel = new JLabel("SELECCIONE USUARIO");
