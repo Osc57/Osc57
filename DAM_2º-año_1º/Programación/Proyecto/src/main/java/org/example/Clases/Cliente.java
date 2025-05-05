ddd3316212d9ee193a49dec473dcdd22681dbb59
Óscar <oubedaf@iesch.org>
2025-05-05 17:36:13 +0200

    clase

---
+++ DAM_2º-año_1º/Programación/Proyecto/src/main/java/org/example/Clases/Cliente.java
@@ -0,0 +1,68 @@
+package org.example.Clases;
+
+public class Cliente {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private int telefono;
+
+    public Cliente(String dni, String nombre, String apellidos, String direccion, int telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono=" + telefono +
+                '}';
+    }
+}
