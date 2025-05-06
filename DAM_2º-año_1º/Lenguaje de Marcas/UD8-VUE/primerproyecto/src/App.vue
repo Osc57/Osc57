bf224f5ab2090e6d11f449188665ab36dc95b011
Óscar <oubedaf@iesch.org>
2025-05-06 21:20:35 +0200

    clase

--- DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
+++ DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
@@ -13,12 +13,18 @@
   <!--
   <h2>{{ esMartes ? "Hoy es Martes" : "Hoy NO es Martes" }}</h2>
   <h2>{{ dias[2] }}</h2>
-  -->
+  
   <h2 v-if="esMartes === true">Es Martes</h2>
   <h2 v-else-if="esMartes === false">NO es Martes</h2>
   <h2 v-else>No tengo NI IDEA de que día es</h2>
 
   <h2 v-show="activo">Estoy en true</h2>
+  -->
+  <ul>
+    <li v-for="(fruta, index) in arrayFrutas :key=index>
+      {{ index }} - {{ fruta }}
+    </li>
+  </ul>
 </template>
 
 <style>
