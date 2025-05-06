960d76bf8cb9196f9c2bf683ebb95598f0a550cd
Óscar <oubedaf@iesch.org>
2025-05-06 20:52:46 +0200

    clase

---
+++ DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const nombre = "Óscar";
+  const color = "color:aqua";
+</script>
+
+<template>
+  <h1 v-bind:style="color">Hola {{ nombre.toUpperCase() }} !!</h1>
+</template>
+
+<style>
+
+</style>
