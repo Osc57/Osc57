c723adb921e55784877dfd730ef8adf5cd92496e
Óscar <oubedaf@iesch.org>
2025-05-06 21:14:45 +0200

    clase

--- DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
+++ DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
   const nombre = "Óscar";
-  const color = "color:aqua";
+  const color = "color:red";
+  const esMartes = null;
+  const dias = ["Lunes","Martes","Miercoles","Miercoles","Jueves","Viernes","Sábado","Domingo"];
+  const activo = false;
+  const arrayFrutas = ["🍎","🍌","🍉","🍓","🍒"];
 </script>
 
 <template>
-  <h1 v-bind:style="color">Hola {{ nombre.toUpperCase() }} !!</h1>
+  
+  <h1 v-bind:style="color">Hola {{ nombre }} !!</h1>
+  <!--
+  <h2>{{ esMartes ? "Hoy es Martes" : "Hoy NO es Martes" }}</h2>
+  <h2>{{ dias[2] }}</h2>
+  -->
+  <h2 v-if="esMartes === true">Es Martes</h2>
+  <h2 v-else-if="esMartes === false">NO es Martes</h2>
+  <h2 v-else>No tengo NI IDEA de que día es</h2>
+
+  <h2 v-show="activo">Estoy en true</h2>
 </template>
 
 <style>
