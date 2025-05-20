7c41b9cfdd8fe41412eb5d7522d8c83ac9c1b0a4
Óscar <oubedaf@iesch.org>
2025-05-20 19:01:14 +0200

    clase

--- DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
+++ DAM_2º-año_1º/Lenguaje de Marcas/UD8-VUE/primerproyecto/src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-  const nombre = "Óscar";
-  const color = "color:red";
-  const esMartes = null;
-  const dias = ["Lunes","Martes","Miercoles","Miercoles","Jueves","Viernes","Sábado","Domingo"];
-  const activo = false;
-  const arrayFrutas = ["🍎","🍌","🍉","🍓","🍒"];
+const nombre = "Óscar";
+const color = "color:red";
+const esMartes = null;
+const dias = ["Lunes", "Martes", "Miercoles", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+const activo = false;
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
 </script>
 
 <template>
-  
+
   <h1 v-bind:style="color">Hola {{ nombre }} !!</h1>
   <!--
   <h2>{{ esMartes ? "Hoy es Martes" : "Hoy NO es Martes" }}</h2>
@@ -21,7 +21,7 @@
   <h2 v-show="activo">Estoy en true</h2>
   -->
   <ul>
-    <li v-for="(fruta, index) in arrayFrutas :key=index>
+    <li v-for="(fruta, index) in arrayFrutas" :key="index">
       {{ index }} - {{ fruta }}
     </li>
   </ul>
