0e554b210e12c839e4bce4455a6227df90a46e8b
Oscar <oubedaf@iesch.org>
2025-10-06 20:56:36 +0200

    clase

--- DAM 3er_año_1º-2025-26/Programación/Prueba/src/Main.java
+++ DAM 3er_año_1º-2025-26/Programación/Prueba/src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hola mundo!");
+        System.out.println("Hola mundo!");
 
+        System.out.println(3000000000000000000l);
     }
 }
