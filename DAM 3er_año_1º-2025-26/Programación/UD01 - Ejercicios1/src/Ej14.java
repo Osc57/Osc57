fa9749ae3b47f47ce860a1b5abd1f61f2843c040
Oscar <oubedaf@iesch.org>
2025-10-08 17:28:07 +0200

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD01 - Ejercicios1/src/Ej14.java
@@ -0,0 +1,19 @@
+
+/*
+14. Realiza un programa con una variable entera t la cual contiene un tiempo en segundos
+y queremos conocer este tiempo expresado en horas, minutos y segundos.
+* */
+
+public class Ej14 {
+    public static void main(String[] args) {
+        int t = 3665;
+
+        int horas = t / 3600;
+        int min = (t % 3600) / 60;
+        int segundos = (t % 3600) % 60;
+
+        System.out.println("Las horas son: " + horas + '\n' +
+                "Los minutos son: " + min + '\n' +
+                "Los segundos son: " + segundos);
+    }
+}
