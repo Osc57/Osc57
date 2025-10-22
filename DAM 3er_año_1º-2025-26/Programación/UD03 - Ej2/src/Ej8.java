0d56a5867beedb39f2a266ca78e1bf7dec9f61a3
Oscar <oubedaf@iesch.org>
2025-10-22 17:30:15 +0200

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej2/src/Ej8.java
@@ -0,0 +1,13 @@
+/*
+8. Crea un programa que escriba en pantalla los números pares del 0 al 12,
+usando "for".
+*/
+
+public class Ej8 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 12; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
