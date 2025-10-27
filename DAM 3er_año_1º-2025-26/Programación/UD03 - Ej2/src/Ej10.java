c196d375f9b880b67971b07746f22de20ef713f4
Oscar <oubedaf@iesch.org>
2025-10-27 20:43:04 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej2/src/Ej10.java
@@ -0,0 +1,26 @@
+/*
+10. Crea un programa que pida al usuario 2 números enteros y calcule la
+multiplicación de ambos aplicando sumas sucesivas del primer número tantas
+veces como valga el segundo. Por ejemplo: 2, 3 → 2 * 3 = 2 + 2 + 2 (3 veces).
+*/
+
+import java.util.Scanner;
+
+public class Ej10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int suma = 0;
+
+        System.out.print("Dime un número: ");
+        int n1 = scanner.nextInt();
+
+        System.out.print("Dime otro número: ");
+        int n2 = scanner.nextInt();
+
+        for (int i = 0; i < n1; i++) {
+            suma = suma + n2;
+        }
+
+        System.out.println(suma);
+    }
+}
---
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej2/src/Ej11.java
@@ -0,0 +1,13 @@
+/*
+11. Crea un programa que pida al usuario 2 números enteros y calcule la división
+de ambos restado al primer número el segundo de manera sucesiva. Por
+ejemplo: 7, 3 → 7 - 3 = 4, 4 – 3 = 1, como 1 es menor que 3 no puedo seguir.
+La división igual a 2 que es el número de veces que se ha podido realizar la
+resta.
+*/
+
+public class Ej11 {
+    public static void main(String[] args) {
+        
+    }
+}
