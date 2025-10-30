cd728b84b1f9d7c773bdc9c8a12c1ec71e4da88c
Oscar <oubedaf@iesch.org>
2025-10-30 17:55:30 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej2/src/Ej16.java
@@ -0,0 +1,29 @@
+/*
+16. Conversor pesetas – euros. Modifica el programa de manera que el programa
+pida una cantidad para convertir a euros, muestre el resultado y vuelva a
+pedir una cantidad para volver a hacerla conversión. Si se introduce un cero el
+programa debe parar. El programa debe devolver la cantidad en euros si es
+positivo, el mensaje de advertencia si es negativo y volver a pedir introducir la
+información tras la operación. Cuando se introduce un cero la ejecución del
+programa debe finalizar.
+*/
+
+import java.util.Scanner;
+
+public class Ej16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long pesetas;
+
+        do {
+            System.out.print("Dime una cantidad de pesetas y pulsa 0 para finalizar: ");
+            pesetas = scanner.nextLong();
+
+            double euros = pesetas / 166.368;
+
+            System.out.printf("La cantidad de euros es: %.2f €%n", euros);
+
+        } while (pesetas > 0);
+    }
+}
