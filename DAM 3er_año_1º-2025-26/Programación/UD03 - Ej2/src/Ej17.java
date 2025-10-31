22b47253255d4457be5113b0200bd6b2938f4994
Oscar <oubedaf@iesch.org>
2025-10-31 19:32:37 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD03 - Ej2/src/Ej17.java
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej2/src/Ej17.java
@@ -21,15 +21,48 @@ public class Ej17 {
         int contador = 0;
         int mayor = Integer.MIN_VALUE;
         int menor = Integer.MAX_VALUE;
+        int mesMayor = 0;
+        int mesMenor = 0;
+        int facturacion;
 
         for (int i = 0; i < 12; i++) {
             System.out.print("Dime cuanto has generado en el mes " + (i + 1) + ": ");
 
-            suma += scanner.nextInt();
-            contador++;
+            if (scanner.hasNextInt()) {
+                facturacion = scanner.nextInt();
+
+                if (facturacion > 0) {
+                    suma += facturacion;
+
+                    if (facturacion > mayor) {
+                        mayor = facturacion;
+                        mesMayor = i + 1;
+                    }
+
+                    if (facturacion < menor) {
+                        menor = facturacion;
+                        mesMenor = i + 1;
+
+                    }
+                    contador++;
+                } else {
+                    System.out.println("ERROR: no puedes facturar 0 o menos");
+                    i--;
+                }
+
+
+            } else {
+                System.out.println("Introduce números enteros");
+                scanner.next();
+                i--;
+            }
+
+
         }
 
-        System.out.println("Total anual: " + suma);
-        System.out.println("Media: " + suma / contador);
+        System.out.println("Total anual: " + suma + "€");
+        System.out.println("Media: " + suma / contador + "€");
+        System.out.println("El mes " + mesMayor + " ha facturado " + mayor + "€ ");
+        System.out.println("El mes " + mesMenor + " ha facturado " + menor + "€ ");
     }
 }
