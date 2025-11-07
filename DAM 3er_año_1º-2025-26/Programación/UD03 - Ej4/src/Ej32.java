276796caaf5ebad50753d87975bb0bf7b8b09a7f
Oscar <oubedaf@iesch.org>
2025-11-07 18:53:54 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD03 - Ej4/src/Ej32.java
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej4/src/Ej32.java
@@ -17,7 +17,7 @@ public class Ej32 {
 
             for (int i = 2; i < n; i++) {
                 if (n % i == 0) {
-                    System.out.println(i + " ");
+                    System.out.print(i + " ");
                 }
             }
 
---
+++ DAM 3er_año_1º-2025-26/Programación/UD03 - Ej4/src/Ej33.java
@@ -0,0 +1,25 @@
+/*
+33. Realiza un programa que pida introducir un número entero por la consola.
+Después deberá mostrar la descomposición del número en factores primos.
+Ejemplo:
+24 | 2
+12 | 2
+6 | 2
+3 | 3
+1
+Tips:
+• Representa el diagrama de flujo de los programas antes de
+desarrollarlos
+• El operador módulo % que calcula el resto sirve para evaluar si un
+número es divisor. El resto de la división es cero si el dividendo es
+múltiplo del divisor.
+• Para evaluar los posibles divisores de un número n basta con evaluar
+divisores hasta n/2, ya que no puede tener divisores mayores que su
+mitad.
+*/
+
+public class Ej33 {
+    public static void main(String[] args) {
+        
+    }
+}
