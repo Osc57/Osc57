e9fdd92b1bde8dd2e44ff6512d4c52fcd96c14d1
Oscar <oubedaf@iesch.org>
2025-11-12 19:17:30 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD04 - Ej1/UD04 - EJ1/src/Ej4.java
+++ DAM 3er_año_1º-2025-26/Programación/UD04 - Ej1/UD04 - EJ1/src/Ej4.java
@@ -7,11 +7,33 @@ para el 3 deberá llegar desde "3x1=3" hasta "3x10=30").
 import java.util.Scanner;
 
 public class Ej4 {
-    public static void escribirTablaMultiplicar(int n) {
+    /*
+    public static String escribirTablaMultiplicar(int n) {
+        String tabla = "";
+
         for (int i = 0; i <= 10; i++) {
             int resultado = n * i;
-            System.out.println(n + " x " + i + " = " + resultado);
+            tabla = tabla + n + " x " + i + " = " + resultado;
+
+            if (i < 10) {
+                tabla = tabla + "\n";
+            }
         }
+        return tabla;
+    }
+    */
+    public static String escribirTablaMultiplicar(int n) {
+        StringBuilder tabla = new StringBuilder();
+
+        for (int i = 0; i <= 10; i++) {
+            int resultado = n * i;
+            tabla.append(n).append(" x ").append(i).append(" = ").append(resultado);
+
+            if (i < 10) {
+                tabla.append("\n");
+            }
+        }
+        return tabla.toString();
     }
 
     public static void main(String[] args) {
@@ -20,6 +42,6 @@ public class Ej4 {
         System.out.print("Di que tabla de multiplicar quieres: ");
         int n = scanner.nextInt();
 
-        escribirTablaMultiplicar(n);
+        System.out.println(escribirTablaMultiplicar(n));
     }
 }
