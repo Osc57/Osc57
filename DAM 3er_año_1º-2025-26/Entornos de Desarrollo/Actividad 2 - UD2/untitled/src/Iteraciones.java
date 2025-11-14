c99301ac7441226d2c0fe78e9fede7fceca1d92d
Oscar <oubedaf@iesch.org>
2025-11-14 21:01:23 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Entornos de Desarrollo/Actividad 2 - UD2/untitled/src/Iteraciones.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Iteraciones {
+    public static void main(String[] args) {
+        // Crear un objeto Random para generar números aleatorios
+        Random random = new Random();
+        // Obtener un número aleatorio entre 1 y 10 (ambos incluidos)
+        int iter = random.nextInt(4); // nextInt(20) genera un número entre 0 y 4
+        int count = random.nextInt(9); // nextInt(20) genera un número entre 0 y 9
+
+        System.out.println("Iteraciones: " + iter + " Números: " + count + ". Cantidad de números a generar: " + iter * count);
+
+        for (int i = 0; i < iter; i++) {
+            System.out.println("Iteración número: " + (i + 1));
+            int j = 0;
+            while (j < count) {
+                System.out.println("-> Iter. " + j + ", valor de i: " + i);
+                j++;
+            }
+        }
+    }
+}
