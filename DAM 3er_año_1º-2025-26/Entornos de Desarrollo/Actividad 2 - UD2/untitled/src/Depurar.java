4c1ccfe21cd858ba4a014901674a21cb564d4fca
Oscar <oubedaf@iesch.org>
2025-11-14 20:22:30 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Entornos de Desarrollo/Actividad 2 - UD2/untitled/src/Depurar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Depurar {
+    public static void main(String[] args) {
+        Scanner NumScanner = new Scanner(System.in);
+
+        int sum = 0, inputNum;
+        int contador = 0;
+        double mean;
+
+        System.out.print("Ingresa el número total de términos que quieres que calcule la media: ");
+        int counter = NumScanner.nextInt();
+        int counter2 = counter;
+
+        for (int x = 0; x < counter; x++) {
+            System.out.print("Por favor ingresa " + (counter2--) + " números: ");
+            inputNum = NumScanner.nextInt();
+
+            sum = sum + inputNum;
+            contador++;
+        }
+
+        mean = (double) sum / contador;
+        System.out.println("La media de " + sum + " los números que ingresaste es " + mean);
+    }
+}
