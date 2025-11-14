3069b7606622acdcf15aafbbe07035db6f498e30
Oscar <oubedaf@iesch.org>
2025-11-14 19:02:03 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD04 - Ej1/UD04 - EJ1/src/Ej11.java
+++ DAM 3er_año_1º-2025-26/Programación/UD04 - Ej1/UD04 - EJ1/src/Ej11.java
@@ -6,12 +6,11 @@ import java.util.Scanner;
 
 public class Ej11 {
 
-    public static int factorialRecursivo(int n){
+    public static int factorialRecursivo(int n) {
         if (n <= 0 || n == 1) {
             return 1;
         }
-
-        return 0;
+        return n * factorialRecursivo(n - 1);
     }
 
     public static void main(String[] args) {
@@ -19,11 +18,11 @@ public class Ej11 {
 
         System.out.print("Dime un número: ");
 
-        if (scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             int n = scanner.nextInt();
 
             System.out.println(factorialRecursivo(n));
-        }else {
+        } else {
             System.out.println("ERROR: Introduce un número entero");
         }
     }
