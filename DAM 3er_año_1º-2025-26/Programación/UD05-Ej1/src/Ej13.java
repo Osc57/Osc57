96a8940ace5771f136f30c276dc5c18c8946a65f
Oscar <oubedaf@iesch.org>
2025-11-21 19:33:18 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD05-Ej1/src/Ej13.java
+++ DAM 3er_año_1º-2025-26/Programación/UD05-Ej1/src/Ej13.java
@@ -5,8 +5,38 @@ esos nombres. Los datos se guardarán en un array. Después el programa
 mostrará la lista ordenada por orden alfabético.
 */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ej13 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        boolean esNumero;
+
+        do {
+            System.out.print("Cuantos nombres vas a introducir: ");
+            if (scanner.hasNextInt()) {
+                int n = scanner.nextInt();
+                String[] nombres = new String[n];
+
+                for (int i = 0; i < nombres.length; i++) {
+                    System.out.print("Vale, dime el " + (i + 1) + " nombre: ");
+                    String nombre = scanner.next();
+                    nombres[i] = nombre;
+                }
+
+                Arrays.sort(nombres);
+
+                for (String name : nombres) {
+                    System.out.print(name + " ");
+                }
+                esNumero = true;
+
+            } else {
+                System.out.println("No pasa nada, recuerda valores enteros (0-9)");
+                scanner.next();
+                esNumero = false;
+            }
+        } while (!esNumero);
     }
 }
