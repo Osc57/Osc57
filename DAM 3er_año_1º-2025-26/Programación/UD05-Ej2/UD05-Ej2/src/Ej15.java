f91afd0662266276064d84662312f6967cd9ce6a
Oscar <oubedaf@iesch.org>
2025-11-27 17:40:32 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD05-Ej2/UD05-Ej2/src/Ej15.java
@@ -0,0 +1,11 @@
+/*
+15. Codifica una función que devuelva una cadena de caracteres con los
+caracteres en el orden inverso al orden original.
+“Hola mundo”  “odnum aloH”.
+*/
+
+public class Ej15 {
+    public static void main(String[] args) {
+        
+    }
+}
