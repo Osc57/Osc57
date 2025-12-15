db7eb800cbeb73c76d9b734e91d02ffad12cadcc
Oscar <oubedaf@iesch.org>
2025-12-15 20:48:40 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD08 - Ej1/src/Ej2.java
@@ -0,0 +1,47 @@
+/*
+2. Crea un programa que pida al usuario introducir una ruta del directorio de
+archivos del ordenador. A continuación, el programa debe mostrar cuál es la
+ruta de la subcarpeta que contiene el archivo o el directorio. También debe
+mostrar si la ruta existe realmente en el ordenador y, en caso de existir, debe
+mostrar si se trata de un fichero o un directorio. En el caso de que se trate de
+un directorio el programa mostrará también el contenido de dicho directorio.
+*/
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dime la ruta tu directorio: ");
+        String ruta = scanner.nextLine();
+
+        Path path = Paths.get(ruta);
+        File file = path.toFile();
+
+        if (file.exists()) {
+            System.out.println("La ruta existe");
+            if (file.isDirectory()) {
+                System.out.println("Es un directorio");
+                String[] contenido = file.list();
+                if (contenido != null && contenido.length > 0) {
+                    for (String elemento : contenido) {
+                        System.out.println(elemento);
+                    }
+                } else {
+                    System.out.println("Directorio vacio");
+                }
+            } else if (file.isFile()) {
+                System.out.println("Es un archivo");
+            }
+        } else {
+            System.out.println("La ruta no existe");
+        }
+
+        scanner.close();
+
+    }
+}
---
+++ DAM 3er_año_1º-2025-26/Programación/UD08 - Ej1/src/Ej3.java
@@ -0,0 +1,11 @@
+/*
+3. Crea en la carpeta Documentos de tu ordenador una subcarpeta llamada
+PRO_UD08_Ej03. Crea manualmente un fichero de texto llamado
+Programacion.txt con el siguiente contenido:
+*/
+
+public class Ej3 {
+    public static void main(String[] args) {
+        
+    }
+}
