938b1800f45c1739d0c81c66806afeabee6763a8
Oscar <oubedaf@iesch.org>
2026-01-19 19:31:07 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/Óscar Úbeda - Practica6/Óscar Úbeda - Prectica6/Oscar Ubeda - Practica6/src/Modelo/PersonalAdministrativo.java
+++ DAM 3er_año_1º-2025-26/Programación/Óscar Úbeda - Practica6/Óscar Úbeda - Prectica6/Oscar Ubeda - Practica6/src/Modelo/PersonalAdministrativo.java
@@ -5,16 +5,18 @@ import java.time.LocalDate;
 public class PersonalAdministrativo extends Trabajador {
     private String departamento;
     private int nivel;
+    private final double COMPLEMENTO = 100.0;
 
     public PersonalAdministrativo(String nombre, String apellidos, String dni, LocalDate fechaDeIncorporacion, double salarioBase, String departamento, int nivel) {
         super(nombre, apellidos, dni, fechaDeIncorporacion, salarioBase);
         this.departamento = departamento;
         this.nivel = nivel;
     }
+
     //Nivel por complemento + salario base
     @Override
     public double calcularSalario() {
-        return super.getSalarioBase();
+        return super.getSalarioBase() + (nivel * COMPLEMENTO);
     }
 
     @Override
