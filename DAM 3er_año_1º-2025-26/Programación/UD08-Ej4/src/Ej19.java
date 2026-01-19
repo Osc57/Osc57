869af88ae6c1c76dd7487676c49d3eed88f18e80
Oscar <oubedaf@iesch.org>
2026-01-19 21:09:32 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD08-Ej4/src/Ej19.java
+++ DAM 3er_año_1º-2025-26/Programación/UD08-Ej4/src/Ej19.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
 19. Validar contraseñas. Escribe una expresión regular para validar contraseñas que
 deben cumplir con los siguientes requisitos:
@@ -8,6 +12,32 @@ deben cumplir con los siguientes requisitos:
 */
 public class Ej19 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        boolean correcto;
+
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[! \"#$%&'()*+,\\-./:;<=>?@^_`{|}]).{8,}$";
+
+        System.out.println("""
+                • Tener al menos 8 caracteres.
+                • Contener al menos una letra mayúscula.
+                • Contener al menos un número.
+                • Contener al menos un carácter especial (@, #, %, etc.).""");
+
+        do {
+            System.out.print("Dime una contraseña para validar: ");
+            String password = scanner.next();
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(password);
+
+            if (matcher.find()) {
+                System.out.println("Contraseña correcta");
+                correcto = true;
+            } else {
+                System.out.println("Contraseña no valida");
+                correcto = false;
+            }
+
+        }while (!correcto);
     }
 }
