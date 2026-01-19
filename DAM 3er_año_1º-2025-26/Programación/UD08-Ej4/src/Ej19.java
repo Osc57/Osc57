62a2ed2baa3a471314605ec061a56ab33bcd4a5c
Oscar <oubedaf@iesch.org>
2026-01-19 21:04:36 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD08-Ej4/src/Ej19.java
@@ -0,0 +1,13 @@
+/*
+19. Validar contraseñas. Escribe una expresión regular para validar contraseñas que
+deben cumplir con los siguientes requisitos:
+• Tener al menos 8 caracteres.
+• Contener al menos una letra mayúscula.
+• Contener al menos un número.
+• Contener al menos un carácter especial (@, #, %, etc.).
+*/
+public class Ej19 {
+    public static void main(String[] args) {
+        
+    }
+}
