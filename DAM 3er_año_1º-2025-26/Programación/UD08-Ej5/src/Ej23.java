1cd8d33be61d97ed3c8e16eca1845f82c238cfee
Oscar <oubedaf@iesch.org>
2026-01-21 17:15:38 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD08-Ej5/src/Ej23.java
@@ -0,0 +1,11 @@
+/*
+23. Reemplazar palabras específicas en un archivo. Desarrolla un programa que
+lea un archivo de texto, reemplace todas las apariciones de una palabra
+específica (por ejemplo, "hola") por otra palabra (por ejemplo, "adiós"), y
+guarde el resultado en un nuevo archivo.
+*/
+public class Ej23 {
+    public static void main(String[] args) {
+        
+    }
+}
