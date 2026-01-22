da4a4beb5fbd01722b3a3a10839928725ffd3fbf
Oscar <oubedaf@iesch.org>
2026-01-22 16:01:10 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD08-Ej5/src/Ej23.java
+++ DAM 3er_año_1º-2025-26/Programación/UD08-Ej5/src/Ej23.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
 23. Reemplazar palabras específicas en un archivo. Desarrolla un programa que
 lea un archivo de texto, reemplace todas las apariciones de una palabra
@@ -6,6 +10,22 @@ guarde el resultado en un nuevo archivo.
 */
 public class Ej23 {
     public static void main(String[] args) {
-        
+        String regex = "\\sy\\s|\\sY\\s";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("archivo.txt"));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("archivoReemplazarPalabras.txt"))) {
+
+            String linea;
+            Pattern pattern = Pattern.compile(regex);
+            while ((linea = bufferedReader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(linea);
+                String reemplazo = matcher.replaceAll(" o ");
+                bufferedWriter.write(reemplazo);
+                bufferedWriter.newLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
