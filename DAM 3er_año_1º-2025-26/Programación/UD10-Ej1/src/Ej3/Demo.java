d36fc0aabc3ccf3c17417b9037541590c720b8a0
Oscar <oubedaf@iesch.org>
2026-01-26 20:01:45 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD10-Ej1/src/Ej3/Demo.java
+++ DAM 3er_año_1º-2025-26/Programación/UD10-Ej1/src/Ej3/Demo.java
@@ -1,4 +1,13 @@
 package Ej3;
 
 public class Demo {
+    public static void main(String[] args) {
+        MetodoPago tarjetaCredito = new TarjetaCredito();
+        MetodoPago payPal = new PayPal();
+        MetodoPago transeferenciaBancaria = new TranseferenciaBancaria();
+
+        System.out.println(tarjetaCredito.procesarPago(700));
+        System.out.println(payPal.procesarPago(450));
+        System.out.println(transeferenciaBancaria.procesarPago(1500));
+    }
 }
