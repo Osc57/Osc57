49e5d239b2e0d54bbac3caa712bbff8d8699ef72
Oscar <oubedaf@iesch.org>
2026-02-02 17:50:32 +0100

    clase

--- DAM 3er_año_1º-2025-26/Entornos de Desarrollo/Entornos/test/CalculatorTest.java
+++ DAM 3er_año_1º-2025-26/Entornos de Desarrollo/Entornos/test/CalculatorTest.java
@@ -7,7 +7,9 @@ class CalculatorTest {
     @Test
     @DisplayName("Add two numbers")
     void add() {
-        assertEquals(4, Calculator.add(2, 2));
+        assertAll( () -> assertEquals(4, Calculator.add(2, 2)),
+                () -> assertEquals(-4,Calculator.multiply(2,-2)));
+
     }
 
     void multiPly() {
