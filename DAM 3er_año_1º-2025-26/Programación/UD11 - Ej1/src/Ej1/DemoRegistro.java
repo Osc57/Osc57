b0bd389422f14482caf2e9bb74cf652425ae9327
Oscar <oubedaf@iesch.org>
2026-02-12 18:21:38 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej1/DemoRegistro.java
+++ DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej1/DemoRegistro.java
@@ -15,31 +15,19 @@ public class DemoRegistro {
                     .toList();
 
             System.out.println("\n--- País: FR ---");
-            registros.stream()
-                    .filter(r -> r.getCountry().equals("FR"))
-                    .forEach(System.out::println);
+            registros.stream().filter(r -> r.getCountry().equals("FR")).forEach(System.out::println);
 
             System.out.println("\n--- Yahoo (Orden Nombre) ---");
-            registros.stream()
-                    .filter(r -> r.getEmail().contains("yahoo"))
-                    .sorted(Comparator.comparing(Registro::getFirstName))
-                    .forEach(System.out::println);
+            registros.stream().filter(r -> r.getEmail().contains("yahoo")).sorted(Comparator.comparing(Registro::getFirstName)).forEach(System.out::println);
 
             System.out.println("\n--- Login Nov15-Ene17 (Desc) ---");
             LocalDateTime inicio = LocalDateTime.of(2015, 11, 1, 0, 0);
             LocalDateTime fin = LocalDateTime.of(2017, 1, 31, 23, 59);
 
-            registros.stream()
-                    .filter(r -> !r.getLastLogin().isBefore(inicio) && !r.getLastLogin().isAfter(fin))
-                    .sorted(Comparator.comparing(Registro::getLastLogin).reversed())
-                    .forEach(System.out::println);
+            registros.stream().filter(r -> !r.getLastLogin().isBefore(inicio) && !r.getLastLogin().isAfter(fin)).sorted(Comparator.comparing(Registro::getLastLogin).reversed()).forEach(System.out::println);
 
             System.out.println("\n--- Empiezan por 'A' (Orden Fecha Desc) ---");
-            registros.stream()
-                    .filter(r -> r.getFirstName().startsWith("A"))
-                    .sorted(Comparator.comparing(Registro::getLastLogin).reversed())
-                    .forEach(r -> System.out.println(r.getFirstName() + " " + r.getLastName()));
-
+            registros.stream().filter(r -> r.getFirstName().startsWith("A")).sorted(Comparator.comparing(Registro::getLastLogin).reversed()).forEach(r -> System.out.println(r.getFirstName() + " " + r.getLastName()));
         } catch (IOException e) {
             System.out.println("Error: Asegúrate de que DATA.csv esté en la carpeta del proyecto.");
         }
