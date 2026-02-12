591629cc267cd78f3c31f0fabcb42a2de7061392
Oscar <oubedaf@iesch.org>
2026-02-12 18:01:09 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej1/DemoRegistro.java
+++ DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej1/DemoRegistro.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class DemoRegistro {
     public static void main(String[] args) {
