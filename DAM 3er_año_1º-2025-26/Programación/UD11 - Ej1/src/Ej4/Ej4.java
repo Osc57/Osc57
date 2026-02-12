276659af1f4934732013afb0d72da3efc2fb1f87
Oscar <oubedaf@iesch.org>
2026-02-12 18:17:23 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej4/Ej4.java
+++ DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej4/Ej4.java
@@ -11,8 +11,37 @@ Por:
 “mouse”
 Lista final: “monitor”, “keyboard”, “mouse”, “computer”
 */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ej4 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> lista = new ArrayList<>();
+
+        System.out.println("Introduce 4 palabras para la lista:");
+        for (int i = 0; i < 4; i++) {
+            lista.add(sc.next());
+        }
+
+        System.out.println("Lista inicial: " + lista);
+
+        System.out.print("\nSustituir la palabra: ");
+        String buscar = sc.next();
+
+        System.out.print("Por: ");
+        String reemplazo = sc.next();
+
+        if (lista.contains(buscar)) {
+            int indice = lista.indexOf(buscar);
+            lista.set(indice, reemplazo);
+            System.out.println("Lista final: " + lista);
+        } else {
+            System.out.println("La palabra '" + buscar + "' no existe en la lista.");
+        }
+
+        sc.close();
     }
 }
+
