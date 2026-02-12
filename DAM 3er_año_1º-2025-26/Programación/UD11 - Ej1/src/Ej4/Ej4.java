18070eec157de0915a1a933ae3411ca437aaaeca
Oscar <oubedaf@iesch.org>
2026-02-12 18:15:25 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej4/Ej4.java
@@ -0,0 +1,18 @@
+package Ej4;
+
+/*
+4. Escribe un programa que permita crear una lista de palabras y que, a continuación,
+pida dos palabras y sustituya la primera por la segunda en la lista.
+Ejemplo:
+Lista inicial: “monitor”, “keyboard”, “trackpad”, “computer”
+Sustituir la palabra:
+“trackpad”
+Por:
+“mouse”
+Lista final: “monitor”, “keyboard”, “mouse”, “computer”
+*/
+public class Ej4 {
+    public static void main(String[] args) {
+        
+    }
+}
