e0e9303fefdcf75f340c8bb1471e69ced11b0402
Oscar <oubedaf@iesch.org>
2026-02-12 18:18:44 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej4/Ej4.java
+++ DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej4/Ej4.java
@@ -15,33 +15,37 @@ Lista final: “monitor”, “keyboard”, “mouse”, “computer”
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ej4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> lista = new ArrayList<>();
 
-        System.out.println("Introduce 4 palabras para la lista:");
-        for (int i = 0; i < 4; i++) {
-            lista.add(sc.next());
-        }
+        // 1. Lista inicial predefinida
+        ArrayList<String> lista = new ArrayList<>(Arrays.asList("monitor", "keyboard", "trackpad", "computer"));
 
         System.out.println("Lista inicial: " + lista);
 
-        System.out.print("\nSustituir la palabra: ");
-        String buscar = sc.next();
+        // 2. Pedir palabras para el cambio
+        System.out.println("Sustituir la palabra:");
+        String buscar = sc.nextLine();
 
-        System.out.print("Por: ");
-        String reemplazo = sc.next();
+        System.out.println("Por:");
+        String reemplazo = sc.nextLine();
 
+        // 3. Sustitución
         if (lista.contains(buscar)) {
             int indice = lista.indexOf(buscar);
             lista.set(indice, reemplazo);
             System.out.println("Lista final: " + lista);
         } else {
-            System.out.println("La palabra '" + buscar + "' no existe en la lista.");
+            System.out.println("La palabra '" + buscar + "' no se encuentra en la lista.");
         }
 
         sc.close();
     }
 }
+
 
