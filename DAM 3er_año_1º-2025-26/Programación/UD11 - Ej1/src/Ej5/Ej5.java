e1b664b65aaa10a43fe6fb4dc500ec431ca13fcd
Oscar <oubedaf@iesch.org>
2026-02-13 19:06:02 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD11 - Ej1/src/Ej5/Ej5.java
@@ -0,0 +1,35 @@
+package Ej5;
+
+/*
+Escribe un programa que permita crear una lista de palabras y que, a continuación,
+pida una palabra al usuario y la elimine de la lista.
+Ejemplo:
+Lista inicial: “monitor”, “keyboard”, “trackpad”, “computer”
+Eliminar la palabra:
+“trackpad”
+Lista final: “monitor”, “keyboard”, “computer”
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ej5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> lista = new ArrayList<>(Arrays.asList("monitor", "keyboard", "trackpad", "computer"));
+
+        System.out.println("Lista inicial: " + lista);
+
+        System.out.print("Eliminar la palabra: ");
+        String eliminar = sc.nextLine();
+
+        if (lista.contains(eliminar)) {
+            lista.remove(eliminar);
+            System.out.println("Lista final: " + lista);
+        } else {
+            System.out.println("La palabra '" + eliminar + "' no se encuentra en la lista.");
+        }
+    }
+}
