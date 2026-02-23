ce4eb1a29df2ad6fad3f1ecbf5c07427ef30f9fd
Oscar <oubedaf@iesch.org>
2026-02-23 19:35:49 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej1y2/Ej1.java
@@ -0,0 +1,28 @@
+package org.example.Ej1y2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/*
+1. Ejercicio 1: Crear una clase “Book” con campos para “title”, “author”, y “yearPublished”.
+Serializar una instancia de “Book” a JSON usando FasterXML.
+*/
+public class Ej1 {
+    public static void main(String[] args) {
+        Book book = new Book("Don Quijote de La Mancha", "Miguel de Cervantes", 1605);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String json = objectMapper.writeValueAsString(book);
+            System.out.println(json);
+
+            objectMapper.writeValue(Paths.get("Libro.json").toFile(), book);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej1y2/Ej2.java
@@ -0,0 +1,26 @@
+package org.example.Ej1y2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+/*
+2. Ejercicio 2: Tomar el JSON producido en el Ejercicio 1 y deserializarlo de nuevo a una
+instancia de “Book” usando FasterXML.
+*/
+public class Ej2 {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Book book = objectMapper.readValue(new File("Libro.json"), Book.class);
+            System.out.println(book);
+            System.out.println("Nombre: " + book.getTitle());
+            System.out.println("Autor: " + book.getAuthor());
+            System.out.println("Año de Lanzamiento: " + book.getYearPublished());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
