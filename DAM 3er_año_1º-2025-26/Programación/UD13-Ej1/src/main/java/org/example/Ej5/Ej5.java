798181f79ea87153c439a281a102352fcaad2168
Oscar <oubedaf@iesch.org>
2026-02-23 20:23:21 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej5/Ej5.java
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej5/Ej5.java
@@ -1,4 +1,25 @@
 package org.example.Ej5;
 
+/*
+5. Ejercicio 5: Repetir los Ejercicios 1 y 2, pero esta vez serializar a XML usando JAXB.
+*/
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
 public class Ej5 {
+    public static void main(String[] args) {
+        Book book = new Book("La Vuelta al Mundo en 80 días", "Julio Verne", 1873);
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(Book.class);
+            
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(book, System.out);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
