68bbbce46b794cff1e4213ac96b3227858a8e6d5
Oscar <oubedaf@iesch.org>
2026-02-23 20:12:20 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej3y4/Ej4.java
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej3y4/Ej4.java
@@ -5,8 +5,25 @@ package org.example.Ej3y4;
 instancia de “Library” usando FasterXML.
 */
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.Ej1y2.Book;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Ej4 {
     public static void main(String[] args) {
-        
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            Library library = objectMapper.readValue(new File("Librería.json"), Library.class);
+
+            for (Book b : library.getListaLibros()) {
+                System.out.println(b);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
