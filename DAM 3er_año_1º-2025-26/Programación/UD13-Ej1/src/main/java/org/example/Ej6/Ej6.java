1ee0b3dd8c03d97e0624a07a75f87916de5769fe
Oscar <oubedaf@iesch.org>
2026-02-25 16:28:28 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej6/Ej6.java
@@ -0,0 +1,29 @@
+package org.example.Ej6;
+
+import org.example.Ej5.Book;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+
+/*
+  Repetir los Ejercicios 3 y 4, pero esta vez serializar a XML usando JAXB.
+*/
+
+public class Ej6 {
+    public static void main(String[] args) {
+        Library library = new Library(new File("Librería.xml"), Library.class);
+        library.meterLibro(new Book("Veinte Mil Leguas de Viaje Submarino", "Julio Verne", 1870));
+        library.meterLibro(new Book("Don Quijote de La Mancha", "Miguel de Cervantes", 1605));
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(Library.class);
+            Marshaller marshaller = context.createMarshaller();
+            
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(library, new File("Libreria.xml"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
