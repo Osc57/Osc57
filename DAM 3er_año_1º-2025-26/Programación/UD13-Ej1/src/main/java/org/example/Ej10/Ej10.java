218fdf0266a407b7237a09f751769530e1aaa475
Oscar <oubedaf@iesch.org>
2026-02-27 16:58:28 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej10/Ej10.java
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej10/Ej10.java
@@ -10,6 +10,33 @@ XML.
 
 public class Ej10 {
     public static void main(String[] args) {
+        LibraryCatalog libraryCatalog = new LibraryCatalog();
 
+        libraryCatalog.anadirLibro(new Author("Julio Verne"), new Book("Veinte Mil Leguas de Viaje Submarino", "Julio Verne", 1870));
+        libraryCatalog.anadirLibro(new Author("Miguel de Cervantes"), new Book("Don Quijote de La Mancha", "Miguel de Cervantes", 1605));
+        libraryCatalog.anadirLibro(new Author("Julio Verne"), new Book("La Vuelta al Mundo en 80 días", "Julio Verne", 1870));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            Map<String, String> map = new HashMap<>();
+            for (Map.Entry<Author, Book> entry : libraryCatalog.getBookMap().entrySet()) {
+                map.put(entry.getKey().getName(), entry.getValue().getTitle());
+                objectMapper.writeValue(Paths.get("LibrayCatalog.json").toFile(), map);
+                System.out.println(map);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(LibraryCatalog.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(libraryCatalog, new File("LibraryCatalog.xml"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
