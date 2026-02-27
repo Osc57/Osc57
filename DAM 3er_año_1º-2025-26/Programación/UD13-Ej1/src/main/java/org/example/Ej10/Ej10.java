c0c3082e33a5655112efe5ef5bad4cbe0266f9c4
Oscar <oubedaf@iesch.org>
2026-02-27 16:49:35 +0100

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej10/Ej10.java
@@ -0,0 +1,15 @@
+package org.example.Ej10;
+
+/*
+10. Ejercicio 10: Crear una clase “LibraryCatalog” que contenga un mapa de “Author” a
+“Book”. Asegurarse de que cuando se serializa una instancia de “LibraryCatalog”, el mapa se
+serializa de manera que cada entrada en el mapa se convierte en un elemento con el nombre del
+autor como la clave y el título del libro como el valor. Hacer esto tanto para JSON como para
+XML.
+*/
+
+public class Ej10 {
+    public static void main(String[] args) {
+        
+    }
+}
