e90b2cbd92bb0141f0a492911bb6b51eaeef517e
Oscar <oubedaf@iesch.org>
2026-02-27 16:57:55 +0100

    clase

--- DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej10/LibraryCatalog.java
+++ DAM 3er_año_1º-2025-26/Programación/UD13-Ej1/src/main/java/org/example/Ej10/LibraryCatalog.java
@@ -3,9 +3,26 @@ package org.example.Ej10;
 import org.example.Ej5.Book;
 import org.example.Ej7.Author;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.HashMap;
 import java.util.Map;
 
+@XmlRootElement
 public class LibraryCatalog {
     Map<Author, Book> mapAuthor = new HashMap<>();
+
+    public LibraryCatalog() {
+    }
+
+    public Map<Author, Book> getmapAuthor() {
+        return mapAuthor;
+    }
+
+    public void setmapAuthor(Map<Author, Book> mapAuthor) {
+        this.mapAuthor = mapAuthor;
+    }
+
+    public void anadirLibro(Author author, Book book){
+        mapAuthor.put(author,book);
+    }
 }
