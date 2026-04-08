8f2bf16048e4246adee15640f587c496440fd541
Oscar <oubedaf@iesch.org>
2026-04-08 16:56:28 +0200

    clase

---
+++ DAM 3er_año_1º-2025-26/Bases de Datos/Tarea Recuperacion Practicas - Veterinaria/Tarea Recuperacion GameWorld - Óscar Úbeda.sql
@@ -0,0 +1,81 @@
+/*TAREA RECUPERACIÓN TIENDA DE VIDEOJUEGOS - ÓSCAR ÚBEDA*/
+/*Creo Base de Datos*/
+CREATE DATABASE GAMEWORLD;
+USE GAMEWORLD;
+CREATE TABLE GENEROS_JUEGO (
+	id_genero INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(40),
+	descripcion VARCHAR(150),
+	multijugador CHAR(1) DEFAULT 'S',
+	
+	CONSTRAINT ck_multijugador CHECK (multijugador IN ('S', 'N'))
+);
+
+CREATE TABLE VIDEOJUEGOS (
+	cod_juego VARCHAR(10) PRIMARY KEY,
+	titulo VARCHAR(100) UNIQUE,
+	desarrollador VARCHAR(80) NOT NULL,
+	anio_lanzamiento INT(4),
+	clasificacion_edad CHAR(5),
+	plataforma VARCHAR(15),
+	precio DECIMAL(6,2),
+	id_genero INT,
+	activo CHAR(1) DAFAULT 'S',
+	
+	CONSTRAINT ck_anio_lanzamiento CHECK (anio_lanzamiento BETWEEN 1970 AND YEAR(CURRENT_DATE) + 1),
+	CONSTRAINT ck_clasificacion_edad CHECK (clasificacion_edad IN ('PEGI3', 'PEGI7', 'PEGI12', 'PEGI16', 'PEGI18')),
+	CONSTRAINT ck_plataforma CHECK (plataforma IN ('PC', 'PS5', 'XBOX', 'SWITCH', 'MOVIL', 'MULTIP')),
+	CONSTRAINT ck_precio CHECK (precio >= 0),
+	CONSTRAINT ck_activo CHECK (activo IN ('S', 'N')),
+	
+	CONSTRAINT fk_videoj_generoj FOREIGN KEY (id_genero) REFERENCES GENEROS_JUEGO(id_genero) ON DELETE RESTRICT
+);
+
+CREATE TABLE JUGADORES (
+	id_jugador INT AUTO_INCREMENT PRIMARY KEY,
+	nickname VARCHAR(30) NOT NULL UNIQUE,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	fecha_registro DATE DEFAULT (CURRENT_DATE),
+	pais VARCHAR(50),
+	nivel_cuenta INT DEFAULT 1,
+	monedas_virtuales DEFAULT 0,
+	estado_cuenta VARCHAR(12) DEFAULT 'ACTIVA',
+	
+	CONSTRAINT ck_nivel_cuenta CHECK (nivel_cuenta > 0),
+	CONSTRAINT ck_monedas_virtuales CHECK (nivel_cuenta >= 0),
+	CONSTRAINT ck_estado_cuenta CHECK (estado_cuenta IN ('ACTIVA', 'SUSPENDIDA', 'BANEADA', 'INACTIVA'))
+);
+
+CREATE TABLE PARTIDAS (
+	id_partida INT AUTO_INCREMENT PRIMARY KEY,
+	cod_juego VARCHAR(10),
+	fecha_inicio DATETIME NOT NULL,
+	fecha_fin DATETIME,
+	modalidad VARCHAR(15),
+	mapa_escenario VARCHAR(60),
+	
+	CONSTRAINT ck_modalidad CHECK (modalidad IN ('INDIVIDUAL', 'COOPERATIVO', 'COMPETITIVO')),
+    CONSTRAINT ck_fecha_fin CHECK (fecha_fin IS NULL OR fecha_fin > fecha_inicio),
+	
+    CONSTRAINT fk_juego_parida FOREIGN KEY (cod_juego) REFERENCES VIDEOJUEGOS(cod_juego) ON DELETE RESTRICT
+);
+
+CREATE TABLE PARTICIPACIONES (
+	id_participacion INT AUTO_INCREMENT PRIMARY KEY,
+	id_partida INT,
+	id_jugador INT,
+	puntuacion INT DEFAULT 0,
+	resultado VARCHAR(10) DEFAULT 'EN_JUEGO',
+	tiempo_jugado_sec INT,
+	bajas_realizadas INT DEFAULT 0,
+	bajas_recibidas INT DEFAULT 0,
+	
+	
+	CONSTRAINT ck_puntuacion CHECK (puntuacion >= 0),
+    CONSTRAINT ck_resultado CHECK (resultado IN ('VICTORIA', 'DERROTA', 'EMPATE', 'EN_JUEGO')),
+    CONSTRAINT ck_tiempo_jugado CHECK (tiempo_jugado_seg >= 0),
+    CONSTRAINT ck_bajas_realiz CHECK (bajas_realizadas >= 0),
+    CONSTRAINT ck_bajas_recib CHECK (bajas_recibidas >= 0),
+	
+	CONSTRAINT 
+);
