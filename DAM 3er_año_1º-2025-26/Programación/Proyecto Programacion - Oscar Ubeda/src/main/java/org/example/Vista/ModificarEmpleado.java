b07c1c57b1f356cffaa45753130e329c19635e25
Oscar <oubedaf@iesch.org>
2026-04-20 21:17:26 +0200

    clase

---
+++ DAM 3er_año_1º-2025-26/Programación/Proyecto Programacion - Oscar Ubeda/src/main/java/org/example/Vista/ModificarEmpleado.java
@@ -0,0 +1,28 @@
+package org.example.Vista;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static org.example.Utils.Estilos.*;
+
+public class ModificarEmpleado extends JFrame {
+    public ModificarEmpleado() {
+        this.setTitle("Modificar Empleado");
+        this.setSize(480, 460);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        configurarCierreVentana(this);
+
+        JLabel introducirCliente = new JLabel("•Seleccione trabajador a modificar");
+        introducirCliente.setFont(FUENTE_TITULO_2);
+        introducirCliente.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 0));
+
+        JPanel panelGestionDni = getjPanelGestionDni();
+        JPanel panelBotonRetorno = getPanelBotonRetorno(this, new GestionEmpleado());
+
+        this.add(introducirCliente, BorderLayout.NORTH);
+        this.add(panelGestionDni, BorderLayout.CENTER);
+        this.add(panelBotonRetorno, BorderLayout.SOUTH);
+    }
+}
