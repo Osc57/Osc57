7ce2bfc2f676b9ca0b7f0dd4af6201f5835d1cad
unknown <oubedaf@iesch.org>
2026-09-11 20:19:42 +0200

    clase

--- DAM_2/Interfaces/EjerciciosPython/ejercicio4.py
+++ DAM_2/Interfaces/EjerciciosPython/ejercicio4.py
@@ -1,15 +1,23 @@
-#Generación de una ventana en python con una clase
+#Generación de una ventana en python con una clase + botón
 
-from PyQt6.QtCore import QSize, QtName;
+from PyQt6.QtCore import QSize;
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton;
 
 class MainWindow(QMainWindow):
-    
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Mi aplicación");
+
+        boton = QPushButton("Pulsa");
+
+        self.setCentralWidget(boton);
+        
 
 
 app = QApplication([]);
 
-window = QMainWindow();
+window = MainWindow();
 
 window.show();
 
