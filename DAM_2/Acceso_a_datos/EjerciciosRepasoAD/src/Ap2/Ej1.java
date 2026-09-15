1aa8aa96824fed0c8a80366aa95d613f6dec8deb
unknown <oubedaf@iesch.org>
2026-09-15 17:51:15 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej1.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej1.java
@@ -1,17 +1,18 @@
 package Ap2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /*
 Ej1. Crear un Set de Strings y añadir elementos a él. Luego, imprimir todos los
 elementos del Set. ¿Qué observas acerca del orden de los elementos?
 */
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Ej1 {
     static void main() {
-        Set<String> stringSet = new HashSet<>();
+        Set<String> stringSet = new HashSet<>(Arrays.asList("Hola", "Que tal", "Como estás", "Saludos", "Bonjour", "Hello"));
 
-        
+        System.out.print(stringSet);
     }
 }
