42da817ee62b3545cf32ce6d5add99e0d232c2be
unknown <oubedaf@iesch.org>
2026-09-15 17:30:03 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej1.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej1.java
@@ -5,8 +5,13 @@ Ej1. Crear un Set de Strings y añadir elementos a él. Luego, imprimir todos lo
 elementos del Set. ¿Qué observas acerca del orden de los elementos?
 */
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Ej1 {
     static void main() {
+        Set<String> stringSet = new HashSet<>();
 
+        
     }
 }
