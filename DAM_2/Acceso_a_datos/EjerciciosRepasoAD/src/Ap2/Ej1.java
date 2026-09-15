0d722a99d2ff0f67d3b82927b58627142c2b2e71
unknown <oubedaf@iesch.org>
2026-09-15 17:27:19 +0200

    clase

---
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej1.java
@@ -0,0 +1,12 @@
+package Ap2;
+
+/*
+Ej1. Crear un Set de Strings y añadir elementos a él. Luego, imprimir todos los
+elementos del Set. ¿Qué observas acerca del orden de los elementos?
+*/
+
+public class Ej1 {
+    static void main() {
+
+    }
+}
