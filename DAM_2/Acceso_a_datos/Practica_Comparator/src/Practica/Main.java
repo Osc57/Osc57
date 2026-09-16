614cc5bb76cb55739b16708ed102aad14ef9fd8f
unknown <oubedaf@iesch.org>
2026-09-16 18:57:46 +0200

    clase

--- DAM_2/Acceso_a_datos/Practica_Comparator/src/Practica/Main.java
+++ DAM_2/Acceso_a_datos/Practica_Comparator/src/Practica/Main.java
@@ -2,6 +2,7 @@ package Practica;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     static void main() {
@@ -12,8 +13,16 @@ public class Main {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         //Primero filtro para ver números pares
-        List<Integer> cuadradosPares = numbers.stream().filter(n -> n % 2 == 0).toList();
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+
+        //List<Integer> cuadradosPares = numbers.stream().filter(n -> n % 2 == 0).toList();
+        List<Integer> cuadradosPares = numbers.stream().filter(n -> n % 2 == 0)
+                .map(n -> n * n).toList();
 
         System.out.println(cuadradosPares);
+
+        List<Integer> numbers2 = Arrays.asList(2, 4, 6, 8, 10);
+
+        System.out.println(numbers2.stream().allMatch(n -> n % 2 == 0));
     }
 }
