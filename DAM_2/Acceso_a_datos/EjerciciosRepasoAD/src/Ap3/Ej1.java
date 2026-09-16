a360151461b1ff4aa5f1258b5fe2362a8f2b8add
unknown <oubedaf@iesch.org>
2026-09-16 16:39:51 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap3/Ej1.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap3/Ej1.java
@@ -1,7 +1,15 @@
 package Ap3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ej1 {
     static void main() {
-        
+        Map<String, String> paisMap = Map.of("España", "Madrid", "Francia", "Paris", "Portugal", "Lisboa", "Noruega", "Oslo", "Alemania", "Berlín");
+
+
+        for (Map.Entry<String, String> entrada : paisMap.entrySet()) {
+            System.out.println("País: " + entrada.getKey() + " -> Capital: " + entrada.getValue());
+        }
     }
 }
