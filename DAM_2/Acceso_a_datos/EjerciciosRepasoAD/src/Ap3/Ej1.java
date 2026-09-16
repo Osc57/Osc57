ac046f028baa389d97a4ef44f1a36ccf8e9a9495
unknown <oubedaf@iesch.org>
2026-09-16 16:41:27 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap3/Ej1.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap3/Ej1.java
@@ -11,7 +11,6 @@ public class Ej1 {
     static void main() {
         Map<String, String> paisMap = Map.of("España", "Madrid", "Francia", "Paris", "Portugal", "Lisboa", "Noruega", "Oslo", "Alemania", "Berlín");
 
-
         for (Map.Entry<String, String> entrada : paisMap.entrySet()) {
             System.out.println("País: " + entrada.getKey() + " -> Capital: " + entrada.getValue());
         }
