92957471046543c44a07ba8265888623af3d1203
unknown <oubedaf@iesch.org>
2026-09-16 17:01:28 +0200

    clase

---
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap3/Ej2.java
@@ -0,0 +1,19 @@
+package Ap3;
+
+/*
+Ej2. Dada una lista de Strings, escribir una función que devuelva un Map donde
+las claves son los Strings únicos de la lista y los valores son el número de veces
+que cada String aparece en la lista.
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Ej2 {
+    static void main() {
+        List<String> stringList = new ArrayList<>(List.of("Hola", "Que tal", "Como estás", "Saludos", "Bonjour", "Hello"));
+
+        
+    }
+}
