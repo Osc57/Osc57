2024d1f44df7384b6ce36076bf8d3ee2e1a84491
unknown <oubedaf@iesch.org>
2026-09-16 18:41:16 +0200

    clase

---
+++ DAM_2/Acceso_a_datos/Practica_Comparator/src/Paciente.java
@@ -0,0 +1,54 @@
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Paciente {
+    private String nombre;
+    private int gravedad;
+    private LocalDateTime llegada;
+
+    public Paciente(String nombre, int gravedad, LocalDateTime llegada) {
+        this.nombre = nombre;
+        this.gravedad = gravedad;
+        this.llegada = llegada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getGravedad() {
+        return gravedad;
+    }
+
+    public void setGravedad(int gravedad) {
+        this.gravedad = gravedad;
+    }
+
+    public LocalDateTime getLlegada() {
+        return llegada;
+    }
+
+    public void setLlegada(LocalDateTime llegada) {
+        this.llegada = llegada;
+    }
+
+    /*
+    public static Comparator<Paciente> comparadorGravedad(List<Paciente> list) {
+
+        for (Paciente p : list) {
+        }
+    }
+    */
+
+    @Override
+    public String toString() {
+        return "Paciente -> " + "Nombre: " + nombre + " - Gravedad: " + gravedad + " - Llegada: " + llegada;
+
+    }
+}
---
+++ DAM_2/Acceso_a_datos/Practica_Comparator/src/Practica/Main.java
@@ -0,0 +1,17 @@
+package Practica;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    static void main() {
+
+    }
+
+    public static void calculaCuadrados() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        //Primero filtro para ver números pares
+        List<Integer> cuadradosPares = numbers.stream().filter(n -> n % 2 == 0).toList();
+    }
+}
