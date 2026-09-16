7faa73e17c01150dcc650f225bede331eaec1cb7
unknown <oubedaf@iesch.org>
2026-09-16 15:50:22 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej3.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ap2/Ej3.java
@@ -6,7 +6,7 @@ public class Ej3 {
     static void main() {
         List<String> stringsArray = new ArrayList<>(Arrays.asList("Hola", "Que tal", "Como estás", "Saludos", "Bonjour", "Hello", "Hola", "Que tal", "Como estás", "Saludos", "Bonjour", "Hello"));
         Set<String> palabrasUnicas = obtenerStringsUnicos(stringsArray);
-        System.out.println("Strings únicos: " + palabrasUnicas);
+        System.out.print("Strings únicos: " + palabrasUnicas);
     }
 
     public static Set<String> obtenerStringsUnicos(List<String> lista) {
