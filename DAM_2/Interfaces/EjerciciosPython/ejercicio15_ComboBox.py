b91580b7a3eb71f0e41d5b1934035d95e1bd0cdc
unknown <oubedaf@iesch.org>
2026-09-18 20:03:04 +0200

    clase

---
+++ DAM_2/Interfaces/EjerciciosPython/ejercicio15_ComboBox.py
@@ -0,0 +1,27 @@
+from PyQt6.QtCore import QSize, Qt;
+from PyQt6.QtWidgets import QApplication, QComboBox, QMainWindow, QCheckBox;
+from PyQt6.QtGui import QPixmap
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Mi aplicación");
+
+        lista = QComboBox();
+
+        lista.addItems(["Uno","Dos","Tres","Cuatro"])
+
+        self.setCentralWidget(lista);
+
+
+    def muestraEstado(self, s):
+        print(s)
+
+app = QApplication([]);
+
+window = MainWindow();
+
+window.show();
+
+app.exec(); #Mantener ventana abierta
