32e032850fa5a478a979d06c622bb333a867f59b
unknown <oubedaf@iesch.org>
2026-09-21 20:47:46 +0200

    clase

---
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Steams/Ej4/Ej4.java
@@ -0,0 +1,14 @@
+package Ejercicios_Steams.Ej4;
+
+/*
+Ejercicio 4:
+Crea la clase persona con al menos los atributos nombre y edad.
+- Ordenar una lista de personas por edad de forma descendente
+- Imprimimos la lista de personas ordenada por nombre de forma ascendente
+*/
+
+public class Ej4 {
+    static void main() {
+        
+    }
+}
