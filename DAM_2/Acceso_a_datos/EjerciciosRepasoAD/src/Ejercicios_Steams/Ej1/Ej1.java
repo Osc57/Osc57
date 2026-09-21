504d9296b15914c5ec07f5c95310375b1eb08d59
unknown <oubedaf@iesch.org>
2026-09-21 20:16:56 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Steams/Ej1/Ej1.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Steams/Ej1/Ej1.java
@@ -7,8 +7,16 @@ Crea una lista con diferentes frutas. Llámala frutería
 Obtén una lista de Strings, que contenga el nombre de las frutas que contenía la frutería.
 */
 
+import java.util.List;
+
 public class Ej1 {
     static void main() {
+        List<Fruta> fruteria = List.of(
+                new Fruta("Banana", "Amarillo"),
+                new Fruta("Plátano", "Amarillo"),
+                new Fruta("Manzana", "Rojo"),
+                new Fruta("Pera", "Verde"));
 
+        fruteria.forEach(f -> System.out.println(f));
     }
 }
--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Steams/Ej1/Fruta.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Steams/Ej1/Fruta.java
@@ -4,6 +4,13 @@ public class Fruta {
     private String nombre;
     private String color;
 
-    
+    public Fruta(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
 
+    @Override
+    public String toString() {
+        return "Fruta -> " + nombre + ", " + color;
+    }
 }
