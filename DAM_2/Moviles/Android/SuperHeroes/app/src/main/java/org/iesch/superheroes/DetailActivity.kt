4161945b18085f15a5745a4ebe6298fe96603dfb
unknown <oubedaf@iesch.org>
2026-09-22 20:24:41 +0200

    clase

--- DAM_2/Moviles/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/DetailActivity.kt
+++ DAM_2/Moviles/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/DetailActivity.kt
@@ -1,6 +1,8 @@
 package org.iesch.superheroes
 
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,21 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Ultimo paso: recibir los datos del main activity
+        val bundle = intent.extras!!
+
+        val superHeroName = bundle.getString("superHeroName") ?: "No hay nombre"
+        val alterEgo = bundle.getString("alterEgo") ?: "No hay alter ego"
+        val bio = bundle.getString("bio") ?: "No hay bio"
+        val power = bundle.getFloat("power", 0.0f)
+
+        //Rellenamos los campos con los valores recibidos
+
+        findViewById<TextView>(R.id.heroName_tv).text = superHeroName
+        findViewById<TextView>(R.id.alter_ego_result).text = alterEgo
+        findViewById<TextView>(R.id.bioResult).text = bio
+        findViewById<RatingBar>(R.id.ratingResult).rating = power
+
     }
 }
