8735921877e962a0151e46a9ec97dbe6bc953e16
unknown <oubedaf@iesch.org>
2026-09-22 17:27:48 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Streams/Ej5/Ej5.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Streams/Ej5/Ej5.java
@@ -11,8 +11,32 @@ empleados de ventas.
 - Dado un nombre de empleado, mostraremos su departamento.
 */
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Ej5 {
     static void main() {
+        List<Empleado> empleados = List.of(
+                new Empleado("Manuel", "Atencion al cliente"),
+                new Empleado("Alejandro", "Contabilidad"),
+                new Empleado("Maria", "Contabilidad"),
+                new Empleado("Alfonso", "Asuntos internos"),
+                new Empleado("Rejina", "Atencion al cliente")
+        );
 
+        Map<String, List<Empleado>> empleadosPorDepartamento = empleados.stream()
+                .collect(Collectors.groupingBy(Empleado::getDepartamento));
+        
+        System.out.println("-------------- Empleados por departamento --------------");
+        System.out.println(empleadosPorDepartamento);
+
+
+        Map<String, Long> conteoPorDepartamento = empleados.stream()
+                .collect(Collectors.groupingBy(Empleado::getDepartamento, Collectors.counting()));
+
+        System.out.println("-------------- Empleados por departamento y cantidad --------------");
+        conteoPorDepartamento.forEach((departamento, cantidad) ->
+                System.out.println(departamento + ": " + cantidad));
     }
 }
