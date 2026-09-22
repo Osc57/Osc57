3169c87a384233b0b558dea5bb4b2bdc952c7a8e
unknown <oubedaf@iesch.org>
2026-09-22 16:37:06 +0200

    clase

--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Streams/Ej4/Ej4.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Streams/Ej4/Ej4.java
@@ -7,8 +7,30 @@ Crea la clase persona con al menos los atributos nombre y edad.
 - Imprimimos la lista de personas ordenada por nombre de forma ascendente
 */
 
+import java.util.Comparator;
+import java.util.List;
+
 public class Ej4 {
     static void main() {
+        List<Persona> personas = List.of(
+                new Persona("Manuel", 34),
+                new Persona("Alberto", 23),
+                new Persona("Javier", 18),
+                new Persona("Maria", 56),
+                new Persona("Jose Javier", 87));
 
+        List<Persona> personasPorEdad = personas.stream()
+                .sorted(Comparator.comparingInt(Persona::getEdad).reversed())
+                .toList();
+
+        System.out.println("---------------- Ordenadas por edad ----------------");
+        personasPorEdad.forEach(System.out::println);
+
+        List<Persona> personasPorNombre = personas.stream()
+                .sorted(Comparator.comparing(Persona::getNombre))
+                .toList();
+
+        System.out.println("---------------- Ordenadas por nombre ----------------");
+        personasPorNombre.forEach(System.out::println);
     }
 }
--- DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Streams/Ej4/Persona.java
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Streams/Ej4/Persona.java
@@ -1,5 +1,26 @@
 package Ejercicios_Streams.Ej4;
 
-public class Persona {
+import java.util.Comparator;
 
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + edad + " años";
+    }
 }
