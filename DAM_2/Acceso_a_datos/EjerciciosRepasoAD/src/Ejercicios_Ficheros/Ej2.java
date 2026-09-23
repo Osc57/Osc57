a9b225565f47703382d3de2f52d0e0d81aed4f3e
unknown <oubedaf@iesch.org>
2026-09-23 16:31:37 +0200

    clase

---
+++ DAM_2/Acceso_a_datos/EjerciciosRepasoAD/src/Ejercicios_Ficheros/Ej2.java
@@ -0,0 +1,13 @@
+package Ejercicios_Ficheros;
+
+/*
+2 Copiar un archivo: Escribir un programa en Java que copie el contenido de un archivo en
+otro archivo. El programa debe tomar como entrada el nombre del archivo de origen y el
+nombre del archivo de destino
+*/
+
+public class Ej2 {
+    static void main() {
+        
+    }
+}
