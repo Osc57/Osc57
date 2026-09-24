4b9f26dbe217e8ee5356712afef5f1cee0d5b916
unknown <oubedaf@iesch.org>
2026-09-24 15:48:33 +0200

    clase

--- DAM_2/Moviles/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/Model/SuperHeroe.kt
+++ DAM_2/Moviles/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/Model/SuperHeroe.kt
@@ -1,11 +1,11 @@
 package org.iesch.superheroes.Model
 
 //1 - Me creo el objeto superheroe y lo hago parcelizable
-@Parceli
+@Parcelize
 data class SuperHeroe(
     val nombre: String,
     val alterEgo: String,
     val bio: String,
     val power: Float
-)
+) : Parcelize
 
